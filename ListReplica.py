a616b28e719944026511adabe57158a81f857112
Ruth Pearson <ruth.pearson8@gmail.com>
2016-02-23 15:17:12 +1100

    added casue of death object and combined mortality rate

--- ListReplica.py
+++ ListReplica.py
@@ -6,19 +6,28 @@ Created on Fri Feb 19 11:33:36 2016
 """
 
 class Compartment:
-    def __init__(self, name, InitialConditions(), Data(), Parameters()):
+    def __init__(self, name, conditions, data, parameters, causeOfDeathList):
         self.name = name  
-        self.InitialConditions = InitialConditions()
-        self.Data = Data()
-        self.Parameters = Parameters()  
+        self.conditions = conditions
+        self.data = data
+        self.parameters = parameters 
+        self.causeOfDeathList = causeOfDeathList
+        self.setOverallMortalityParameterFromDeathList()
+        
+    def setOverallMortalityParameterFromDeathList(self):
+        combinedRate = 0
+        for cause in self.causeOfDeathList:
+            combinedRate += cause.mortalityRate #THIS IS NOT THE RIGHT WAY TO COMBINE MORTALITIES - PLACE HOLDER
+        self.parameters.mortalityStunted = self.parameters.stuntingMortalityFactor * combinedRate
+        self.parameters.mortalityNonStunted = combinedRate
        
-    def UpdateCompartment(self):
-        InitialConditions.applyMortality(Parameters.mortalityStunted, Parameters.mortalityNotStunted)
-        InitialConditions.applyNewStunting(Parameters.stuntingRate)
-        InitialConditions.applyRecovery(Parameters.recoveryRate)
+    def updateCompartment(self):
+        self.conditions.applyMortality(self.parameters.mortalityStunted, self.parameters.mortalityNotStunted)
+        self.conditions.applyNewStunting(self.parameters.stuntingRate)
+        self.conditions.applyRecovery(self.parameters.recoveryRate)
         
         
-class InitialConditions:
+class Conditions:
     def __init__(self,stuntedPopulationSize, nonStuntedPopulationSize):
         self.stuntedPopulationSize = stuntedPopulationSize
         self.nonStuntedPopulationSize = nonStuntedPopulationSize 
@@ -36,21 +45,71 @@ class InitialConditions:
         recoveryCases = self.stuntedPopulationSize * recoveryRate
         self.stuntedPopulationSize -= recoveryCases
         self.nonStuntedPopulationSize += recoveryCases
-        
-   
-    
+         
+         
 class Data:
-    def __init__(self,totalPopulationSize, percentStunted, mortalityPerMonth):
+    def __init__(self,totalPopulationSize, percentStunted, mortalityPerMonth, listNumberDeathsByCause):
         self.totalPopulationSize = totalPopulationSize
         self.percentStunted = percentStunted
-        self.mortalityPerMonth = mortalityPerMonth
+        self.listNumberDeathsByCause = listNumberDeathsByCause #ordering matches casue of death list for this age group
+        
+        
         
 class Parameters:
-    def __init__(self, stuntingRate, recoveryRate, mortalityStunted, mortalityNotStunted, agingRate):
+    def __init__(self, stuntingRate, recoveryRate, agingRate, stuntingMortalityFactor):
         self.stuntingRate = stuntingRate
         self.recoveryRate = recoveryRate
-        self.mortalityStunted = mortalityStunted
-        self.mortalityNotStunted = mortalityNotStunted
+        self.mortalityStunted = 0
+        self.mortalityNotStunted = 0
         self.agingRate = agingRate
+        self.stuntingMortalityFactor = stuntingMortalityFactor
+        
    
    
+class FertileWomen:
+    def __init__(self, birthRateStunted, birthRateNonStunted, stuntedPopulationSize, nonStuntedPopulationSize):    
+        self.birthRateStunted = birthRateStunted
+        self.bithRateNonStunted = birthRateNonStunted
+        self.stuntedPopulationSize = stuntedPopulationSize
+        self.nonStuntedPopulationSize = nonStuntedPopulationSize
+ 
+ 
+class Model:
+    def __init__(self, fertileWomen, compartmentList):
+        self.fertileWomen = fertileWomen
+        self.compartmentList = compartmentList
+        
+    def moveOneTimeStep(self):
+        #call update on compartment list
+        for group in self.compartmentList:
+            group.updateCompartment()
+        
+#        #perform aging
+#        #new births first
+#        saveThisPopulationSizeStunted = self.compartmentList[0].conditions.stuntedPopulationSize
+#        saveThisPopulationSizeNonStunted = self.compartmentList[0].conditions.nonStuntedPopulationSize        
+#        
+#        self.compartmentList[0].conditions.stuntedPopulationSize += self.fertileWomen.birthRateStunted * self.fertileWomen.stuntedPopulationSize    
+#        self.compartmentList[0].conditions.nonStuntedPopulationSize += self.fertileWomen.bithRateNonStunted * self.fertileWomen.nonStuntedPopulationSize        
+#        
+#        #all the other compartments
+#        for group in range(1, len(self.compartmentList)):
+#            #USE SAVED VALUES   
+#            #make this better   
+#            addThis = self.compartmentList[group-1].conditions.stuntedPopulationSize * self.compartmentList[group-1].parameters.agingRate
+#            minusThis =  self.compartmentList[group].conditions.stuntedPopulationSize * self.compartmentList[group].parameters.agingRate           
+#            #need to do this for stunted and non-stunted
+#            self.compartmentList[group] += addThis - minusThis
+#            
+    
+    
+class CauseOfDeath:
+    def __init__(self,name, mortalityRate):
+        self.name = name
+        self.mortalityRate = mortalityRate 
+        
+        
+    
+    
+    
+    
--- RunTheModel.py
+++ RunTheModel.py
@@ -7,20 +7,28 @@ Created on Fri Feb 19 12:06:58 2016
 
 import ListReplica as code
 
+cause1 = code.CauseOfDeath("cause 1", 0.1)
+cause2 = code.CauseOfDeath("cause 2", 0.2)
+cause3 = code.CauseOfDeath("cause 3", 0.3)
+
+listDeathCausesCompartment1 = [cause1, cause2, cause3]
+listDeathCausesCompartment2 = [cause1, cause2, cause3]
+listDeathCausesCompartment3 = [cause1, cause2, cause3]
+
 conds1 = code.Conditions(30, 40)
-d1 = code.Data(80, 0.2,11)
-params1 = code.Parameters(0.1, 0.05, 0.3, 0.2, 0.1)
-compartment1 = code.Compartment("compartment 1", conds1, d1, params1)
+d1 = code.Data(80, 0.2, 11, [2, 6, 7])
+params1 = code.Parameters(0.1, 0.05, 0.3, 0.2)
+compartment1 = code.Compartment("compartment 1", conds1, d1, params1, listDeathCausesCompartment1)
 
 conds2 = code.Conditions(30, 40)
-d2 = code.Data(80, 0.2,11)
-params2 = code.Parameters(0.1, 0.05, 0.3, 0.2, 0.1)
-compartment2 = code.Compartment("compartment 2", conds2, d2, params2)
+d2 = code.Data(80, 0.2, 11, [5, 9, 20])
+params2 = code.Parameters(0.1, 0.05, 0.3, 0.2)
+compartment2 = code.Compartment("compartment 2", conds2, d2, params2, listDeathCausesCompartment1)
 
 conds3 = code.Conditions(30, 40)
-d3 = code.Data(80, 0.2,11)
-params3 = code.Parameters(0.1, 0.05, 0.3, 0.2, 0.1)
-compartment3 = code.Compartment("compartment 3", conds3, d3, params3)
+d3 = code.Data(80, 0.2, 11, [10, 4, 9])
+params3 = code.Parameters(0.1, 0.05, 0.3, 0.2)
+compartment3 = code.Compartment("compartment 3", conds3, d3, params3, listDeathCausesCompartment1)
 
 compartmentList = [compartment1, compartment2, compartment3]
 
@@ -31,6 +39,7 @@ testModel.moveOneTimeStep()
 
 for thing in testModel.compartmentList:
     print thing.conditions.stuntedPopulationSize
+    print thing.parameters.mortalityStunted
 
 #compartment.updateCompartment()
 #print compartment.conditions.stuntedPopulationSize
