816eae0b4d317295409f4d2bb5275617f25ad52b
“DrMud” <madhura.killedar.astro@gmail.com>
2016-03-01 13:05:39 +1100

    add birthCircumstanceDist, timeBetweenBirthsDist

--- fake_data.py
+++ fake_data.py
@@ -6,7 +6,7 @@ Created on Fri Feb 26 15:57:07 2016
 """
 
 class Data:
-    def __init__(self, ages, causesOfDeath, totalMortalityByAge, causeOfDeathByAge, RRStunting, RRWasting, RRBreastFeeding, stuntingDistribution, wastingDistribution, breastFeedingDistribution):
+    def __init__(self, ages, causesOfDeath, totalMortalityByAge, causeOfDeathByAge, RRStunting, RRWasting, RRBreastFeeding, stuntingDistribution, wastingDistribution, breastfeedingDistribution, birthCircumstanceDist, timeBetweenBirthsDist):
         self.ages = ages
         self.causesOfDeath = causesOfDeath
         self.totalMortalityByAge = totalMortalityByAge
@@ -16,7 +16,9 @@ class Data:
         self.RRBreastFeeding = RRBreastFeeding
         self.stuntingDistribution = stuntingDistribution
         self.wastingDistribution = wastingDistribution
-        self.breastFeedingDistribution = breastFeedingDistribution
+        self.breastfeedingDistribution = breastfeedingDistribution
+        self.birthCircumstanceDist = birthCircumstanceDist
+        self.timeBetweenBirthsDist = timeBetweenBirthsDist
     
     
     
@@ -50,8 +52,16 @@ def getFakeData():
     #stunting, wasting, breast feeding distributions (similar form to one disease table of RR, so just re-use for now)
     stuntingDistribution = RRStuntingMalaria
     wastingDistribution = RRStuntingMalaria
-    breastFeedingDistribution = RRBreastFeedingMalaria
+    breastfeedingDistribution = RRBreastFeedingMalaria
     
-    fakeData = Data(ages, causesOfDeath, totalMortalityByAge, causeOfDeathByAge, RRStunting, RRWasting, RRBreastFeeding, stuntingDistribution, wastingDistribution, breastFeedingDistribution)
+    # birth circumstance distributions
+    birthCircumstanceDist = {"<18 years":{"first":0.0543,"second or third":0.009,"greater than third":0.00},
+                             "18-34 years":{"first":0.1711,"second or third":0.3607,"greater than third":0.2908},
+                             "35-49 years":{"first":0.0003,"second or third":0.0085,"greater than third":0.1048}}
+
+    # distribution of time between births
+    timeBetweenBirthsDist = {"first":0.2258,"<18 months":0.0705,"18-23 months":0.134,"<24 months":0.5698}
+
+    fakeData = Data(ages, causesOfDeath, totalMortalityByAge, causeOfDeathByAge, RRStunting, RRWasting, RRBreastFeeding, stuntingDistribution, wastingDistribution, breastfeedingDistribution, birthCircumstanceDist, timeBetweenBirthsDist)
     return fakeData
     
