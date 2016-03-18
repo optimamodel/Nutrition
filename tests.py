82594385d71a6702a02dc0990f2895dd525b99c6
Ruth Pearson <ruth.pearson8@gmail.com>
2016-03-18 12:19:02 +1100

    wrote some more model tests

--- tests.py
+++ tests.py
@@ -46,6 +46,8 @@ def setUpDataAndModelObjects():
     testModel = model.Model("Main model", mothers, listOfAgeCompartments, testData.ages, timestep)
     # make the constants object    
     testConstants = constants.Constants(testData, testModel)
+    # set the constants in the model object
+    testModel.setConstants(testConstants)
     return testData, testModel, testConstants
     
 
@@ -61,14 +63,14 @@ class TestsForConstantsClass(unittest.TestCase):
         # for OR = 2, assuming F(a) = F(a-1) = 0.5:
         # pn = sqrt(2) - 1
         for age in ['1-5 months', '6-11 months', '12-23 months', '24-59 months']:
-            self.assertEqual(self.testConstants.probStuntedIfNotPreviously[age], numpy.sqrt(2)-1)
+            self.assertAlmostEqual(self.testConstants.probStuntedIfNotPreviously[age], numpy.sqrt(2)-1)
         
     def testRealtionshipBetweenStuntingProbabilitiesWhenORis2(self):
         # this relationship between ps and pn comes from the OR definition
         # ps = OR * pn / (1 - pn + (OR * pn)) 
         for age in ['1-5 months', '6-11 months', '12-23 months', '24-59 months']:
             ps = 2 * self.testConstants.probStuntedIfNotPreviously[age] / (1 + self.testConstants.probStuntedIfNotPreviously[age])
-            self.assertEqual(self.testConstants.probStuntedIfPreviously[age], ps)
+            self.assertAlmostEqual(self.testConstants.probStuntedIfPreviously[age], ps)
                 
     @unittest.skip("write test once quartic is solved")            
     def testGetBaselineBirthOutcome(self):
@@ -101,9 +103,37 @@ class TestsForModelClass(unittest.TestCase):
                         sumCumulativeDeaths += self.testModel.listOfAgeCompartments[ageGroup].dictOfBoxes[stuntingStatus][wastingStatus][breastfeedingStatus].cumulativeDeaths
             self.assertAlmostEqual(64. * (100./12.), sumCumulativeDeaths)
             self.assertAlmostEqual(64. * (100. - (100./12.)), sumPopSize)
+            
+    def testApplyAgingForNewbornsOnly(self):
+        self.testModel.applyAging()
+        self.assertEqual(100. - int(100./12.), self.testModel.listOfAgeCompartments[0].dictOfBoxes['mild']['high']['predominant'].populationSize)
         
-    
-    
+    def testApplyAgingForIntegerProbabilities(self):
+        # sum aging out age[0] should equal sum aging in age[1]   
+        # set integer probabilities so that there is no movement between stunting statuses
+        self.testModel.constants.probStuntedIfNotPreviously['1-5 months'] = 0
+        self.testModel.constants.probStuntedIfPreviously['1-5 months'] = 1
+        sumAgeingOutAge0 = 64. * int(100. * (1./12))
+        sumAgeingOutAge1 = 64. * int(100. * (1./5) * (1./12))
+        self.testModel.applyAging()
+        sumPopAge1 = 0
+        for stuntingStatus in ["normal", "mild", "moderate", "high"]:
+                for wastingStatus in ["normal", "mild", "moderate", "high"]:
+                    for breastfeedingStatus in ["exclusive", "predominant", "partial", "none"]:
+                        sumPopAge1 += self.testModel.listOfAgeCompartments[1].dictOfBoxes[stuntingStatus][wastingStatus][breastfeedingStatus].populationSize 
+        self.assertEqual(sumPopAge1, (64 * 100) - sumAgeingOutAge1 + sumAgeingOutAge0)    
+
+    @unittest.skip("talk to Mud about this")
+    def testApplyAgingForNonIntegerProbabilities(self):
+        # need to write this test
+        # sum aging out age[0] should equal sum aging in age[1]
+         self.assertTrue(False)
+         
+    @unittest.skip("need to translate birth outcome to stunting after quartic solved")     
+    def testApplyBirths(self):
+        # need to write this test
+        self.assertTrue(False)
+         
     
 # this needs to be here for the tests to run automatically    
 if __name__ == '__main__':
