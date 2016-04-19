5d3d7f66a44f3945f21713a1632d4ea6f2d514ba
“DrMud” <madhura.killedar.astro@gmail.com>
2016-04-19 11:38:39 +1000

    feed in constants and keyList; function to increaseCoverageOfZinc

--- parameters.py
+++ parameters.py
@@ -4,12 +4,16 @@ Created on Fri April 1 2016
 
 @author: madhura
 """
+from __future__ import division
 
 class Params:
-    def __init__(self, data):
-        #self.ages = data.ages
+    def __init__(self, data, constants, keyList):
+        self.constants = constants
+        self.ages,self.birthOutcomes,self.wastingList,self.stuntingList,self.breastfeedingList = keyList
+        import helper as helperCode
+        self.helper = helperCode.Helper()
+
         self.causesOfDeath = data.causesOfDeath
-        #self.totalMortality = data.totalMortality
         self.causeOfDeathDist = data.causeOfDeathDist
         self.stuntingDistribution = data.stuntingDistribution
         self.wastingDistribution = data.wastingDistribution
@@ -28,7 +32,21 @@ class Params:
         self.RRbirthOutcomeByTime = data.RRbirthOutcomeByTime
         self.ORBirthOutcomeStunting = data.ORBirthOutcomeStunting
         self.birthOutcomeDist = data.birthOutcomeDist
+        self.ORstuntingZinc = data.ORstuntingZinc
+        self.InterventionCoverages = data.InterventionCoveragesCurrent
     
 
 # Add all functions for updating parameters due to interventions here....
 
+    def increaseCoverageOfZinc(self,newCoverage):
+        oldCoverage = self.InterventionCoverages["Zinc supplementation"]
+        for ageName in self.ages:
+            probStuntingIfZinc = self.constants.fracStuntedIfZinc["zinc"][ageName]
+            probStuntingIfNoZinc = self.constants.fracStuntedIfZinc["nozinc"][ageName]
+            #oldProbStunting = 
+            newProbStunting = newCoverage*probStuntingIfZinc + (1.-newCoverage)*probStuntingIfNoZinc
+            self.stuntingDistribution[ageName] = self.helper.restratify(newProbStunting)
+        affectedFrac = 0.253
+        effectiveness = 0.65
+        reduction = affectedFrac * effectiveness * (newCoverage - oldCoverage) / (1. - effectiveness*oldCoverage)
+        self.InciDiarrhoea["12-23 months"] *= (1.-reduction)
