248d01801e8c35b09d20a11cdfb308706c32bfce
“DrMud” <madhura.killedar.astro@gmail.com>
2016-04-22 20:17:29 +1000

    changes names of none and some to default and zinc

--- simpleIntervention.py
+++ simpleIntervention.py
@@ -8,26 +8,27 @@ Created on Tue Mar 29 16:14:56 2016
 import pickle as pickle
 import output as output
 
-file1 = open('noIntervention.pkl', 'rb')
+file1 = open('testDefault.pkl', 'rb')
 # read the model output from no interventions
-none = []
+default = []
 while 1:
     try:
-        none.append(pickle.load(file1))
+        default.append(pickle.load(file1))
     except (EOFError):
         break
 file1.close()
 
-file2 = open('someIntervention.pkl', 'rb')
+file2 = open('testZinc.pkl', 'rb')
 # read the model output with simple intervention
-some = []
+zinc = []
 while 1:
     try:
-        some.append(pickle.load(file2))
+        zinc.append(pickle.load(file2))
     except (EOFError):
         break
 file2.close()
 
+"""
 file3 = open('extremeIntervention.pkl', 'rb')
 # read the model output with simple intervention
 extreme = []
@@ -37,21 +38,20 @@ while 1:
     except (EOFError):
         break
 file3.close()
+"""
 
 
+output.getPopSizeByAgePlot(default, "default")
+output.getPopSizeByAgePlot(zinc, "zinc")
 
-output.getPopSizeByAgePlot(none, "none")
-output.getPopSizeByAgePlot(some, "some")
-output.getPopSizeByAgePlot(extreme, "extreme")
+output.getPopAndStuntedSizePlot(default, "default")
+output.getPopAndStuntedSizePlot(zinc, "zinc")
 
-output.getCumulativeDeathsByAgePlot(none, "none")
-output.getCumulativeDeathsByAgePlot(some, "some")
-output.getCumulativeDeathsByAgePlot(extreme, "extreme")
+output.getCumulativeDeathsByAgePlot(default, "default")
+output.getCumulativeDeathsByAgePlot(zinc, "zinc")
 
-output.getNumStuntedByAgePlot(none, "none")
-output.getNumStuntedByAgePlot(some, "some")
-output.getNumStuntedByAgePlot(extreme, "extreme")
+output.getNumStuntedByAgePlot(default, "default")
+output.getNumStuntedByAgePlot(zinc, "zinc")
 
-output.getStuntedPercent(none, "none")
-output.getStuntedPercent(some, "some")
-output.getStuntedPercent(extreme, "extreme")
+output.getStuntedPercent(default, "default")
+output.getStuntedPercent(zinc, "zinc")
