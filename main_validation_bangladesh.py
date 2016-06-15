0f5888e4d0f903aa5658eea6f29dc43b16b94e10
“DrMud” <madhura.killedar.astro@gmail.com>
2016-06-15 16:44:36 +1000

    test historical validation, but no changes to coverage

---
+++ main_validation_bangladesh.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed June 01 2016
+
+@author: madhurakilledar
+"""
+from __future__ import division
+
+import data as dataCode
+import helper as helper
+import output as output
+from copy import deepcopy as dcp
+import pickle as pickle
+
+country = 'Bangladesh'
+
+helper = helper.Helper()
+ages = ["<1 month", "1-5 months", "6-11 months", "12-23 months", "24-59 months"]
+birthOutcomes = ["Pre-term SGA", "Pre-term AGA", "Term SGA", "Term AGA"]
+wastingList = ["normal", "mild", "moderate", "high"]
+stuntingList = ["normal", "mild", "moderate", "high"]
+breastfeedingList = ["exclusive", "predominant", "partial", "none"]
+keyList = [ages, birthOutcomes, wastingList, stuntingList, breastfeedingList]
+
+dataFilename = 'InputForCode_%s_2000.xlsx'%(country)
+spreadsheetData = dataCode.getDataFromSpreadsheet(dataFilename, keyList)
+mothers = helper.makePregnantWomen(spreadsheetData)
+mothers['annualPercentPopGrowth'] = -0.01
+ageGroupSpans = [1., 5., 6., 12., 36.] # number of months in each age group
+agingRateList = [1./1., 1./5., 1./6., 1./12., 1./36.] # fraction of people aging out per MONTH
+numAgeGroups = len(ages)
+agePopSizes  = helper.makeAgePopSizes(numAgeGroups, ageGroupSpans, spreadsheetData)
+
+timestep = 1./12. 
+numsteps = 180
+timespan = timestep * float(numsteps)
+
+for intervention in spreadsheetData.interventionList:
+    print "Baseline coverage of %s = %g"%(intervention,spreadsheetData.interventionCoveragesCurrent[intervention])
+
+# initialise
+newCoverages={}
+for intervention in spreadsheetData.interventionList:
+    newCoverages[intervention] = spreadsheetData.interventionCoveragesCurrent[intervention]
+
+plotData = []
+run = 0
+
+#------------------------------------------------------------------------    
+# DEFAULT RUN WITH NO CHANGES TO INTERVENTIONS
+nametag = "Optima model"
+filenamePrefix = '%s_Historical'%(country)
+pickleFilename = '%s.pkl'%(filenamePrefix)
+plotcolor = 'grey'
+
+print "\n"+nametag
+model, constants, params = helper.setupModelConstantsParameters(nametag, mothers, timestep, agingRateList, agePopSizes, keyList, spreadsheetData)
+
+totalStepsTaken = 0
+# file to dump objects into at each time step
+outfile = open(pickleFilename, 'wb')
+for t in range(100):
+    model.moveOneTimeStep()
+    pickle.dump(model, outfile)
+totalStepsTaken += t+1
+
+# update coverages
+#newCoverages[chosenIntervention] = ??
+#newCoverages[chosenIntervention] = min(newCoverages[chosenIntervention],spreadsheetData.interventionCostCoverage[chosenIntervention]["saturation coverage"])
+#newCoverages[chosenIntervention] = max(newCoverages[chosenIntervention],0.0)
+#model.updateCoverages(newCoverages)
+
+# Run model
+for t in range(numsteps-totalStepsTaken):
+    model.moveOneTimeStep()
+    pickle.dump(model, outfile)
+totalStepsTaken += t+1
+
+# done
+outfile.close()    
+
+# collect output, make graphs etc.
+infile = open(pickleFilename, 'rb')
+modelList = []
+while 1:
+    try:
+        modelList.append(pickle.load(infile))
+    except (EOFError):
+        break
+infile.close()
+
+plotData.append({})
+plotData[run]["modelList"] = modelList
+plotData[run]["tag"] = nametag
+plotData[run]["color"] = plotcolor
+run += 1
+
+
+#------------------------------------------------------------------------    
+
+
+output.getCombinedPlots(run, plotData, startYear=2015, filenamePrefix=filenamePrefix, save=True)
+#output.getDeathsAverted(modelList, newModelList, 'test')
+#output.getStuntedPercent(modelList, '2000-2015', startYear=2000)
+
+
