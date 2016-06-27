1f6fde306f859001777689c87db8be2381f5455f
Ruth Pearson <ruth.pearson8@gmail.com>
2016-06-27 15:46:40 +1000

    updated script to get current national and geospatial budgets. put them in a file to send Kelsey for plotting

---
+++ Kelsey_plotting/current_budgets_270616.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 27 15:40:06 2016
+
+@author: ruth
+"""
+
+from numpy import array
+
+# NATIONAL
+initialAllocation = {u'Balanced energy supplementation': array([ 0.]),
+ u'Breastfeeding promotion (dual delivery)': array([ 18421080.02984185]),
+ u'Complementary feeding (education)': array([ 4554969.38239598]),
+ u'Complementary feeding (supplementation)': array([ 0.]),
+ u'Multiple micronutrient supplementation': array([ 0.]),
+ u'Vitamin A supplementation': array([ 3775164.90276627]),
+ u'Zinc supplementation': array([ 0.])}
+
+# GEOSPATIAL
+geospatialInitialAllocation = [{'region 0': {u'Balanced energy supplementation': array([ 0.]),
+   u'Breastfeeding promotion (dual delivery)': array([ 1051427.57112303]),
+   u'Complementary feeding (education)': array([ 185396.55699531]),
+   u'Complementary feeding (supplementation)': array([ 0.]),
+   u'Multiple micronutrient supplementation': array([ 0.]),
+   u'Vitamin A supplementation': array([ 203646.37961313]),
+   u'Zinc supplementation': array([ 0.])}},
+ {'region 1': {u'Balanced energy supplementation': array([ 0.]),
+   u'Breastfeeding promotion (dual delivery)': array([ 3676834.7429759]),
+   u'Complementary feeding (education)': array([ 1018278.72799817]),
+   u'Complementary feeding (supplementation)': array([ 0.]),
+   u'Multiple micronutrient supplementation': array([ 0.]),
+   u'Vitamin A supplementation': array([ 816285.01085529]),
+   u'Zinc supplementation': array([ 0.])}},
+ {'region 2': {u'Balanced energy supplementation': array([ 0.]),
+   u'Breastfeeding promotion (dual delivery)': array([ 6040563.49472205]),
+   u'Complementary feeding (education)': array([ 2049857.39192701]),
+   u'Complementary feeding (supplementation)': array([ 0.]),
+   u'Multiple micronutrient supplementation': array([ 0.]),
+   u'Vitamin A supplementation': array([ 1234583.59517784]),
+   u'Zinc supplementation': array([ 0.])}},
+ {'region 3': {u'Balanced energy supplementation': array([ 0.]),
+   u'Breastfeeding promotion (dual delivery)': array([ 1983513.22657349]),
+   u'Complementary feeding (education)': array([ 695074.48399454]),
+   u'Complementary feeding (supplementation)': array([ 0.]),
+   u'Multiple micronutrient supplementation': array([ 0.]),
+   u'Vitamin A supplementation': array([ 434228.62120779]),
+   u'Zinc supplementation': array([ 0.])}},
+ {'region 4': {u'Balanced energy supplementation': array([ 0.]),
+   u'Breastfeeding promotion (dual delivery)': array([ 2346827.72900638]),
+   u'Complementary feeding (education)': array([ 775394.13698408]),
+   u'Complementary feeding (supplementation)': array([ 0.]),
+   u'Multiple micronutrient supplementation': array([ 0.]),
+   u'Vitamin A supplementation': array([ 415438.88300409]),
+   u'Zinc supplementation': array([ 0.])}},
+ {'region 5': {u'Balanced energy supplementation': array([ 0.]),
+   u'Breastfeeding promotion (dual delivery)': array([ 2023753.75690625]),
+   u'Complementary feeding (education)': array([ 964185.08915818]),
+   u'Complementary feeding (supplementation)': array([ 0.]),
+   u'Multiple micronutrient supplementation': array([ 0.]),
+   u'Vitamin A supplementation': array([ 455676.05284903]),
+   u'Zinc supplementation': array([ 0.])}},
+ {'region 6': {u'Balanced energy supplementation': array([ 0.]),
+   u'Breastfeeding promotion (dual delivery)': array([ 1298159.50853474]),
+   u'Complementary feeding (education)': array([ 262348.18946195]),
+   u'Complementary feeding (supplementation)': array([ 0.]),
+   u'Multiple micronutrient supplementation': array([ 0.]),
+   u'Vitamin A supplementation': array([ 223251.13279473]),
+   u'Zinc supplementation': array([ 0.])}}]
