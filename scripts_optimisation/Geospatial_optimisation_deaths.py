3cad556410c6242b96dce6d4feeebd8044508056
Ruth Pearson <ruth.pearson8@gmail.com>
2016-07-07 17:24:50 +1000

    updated scripts to reflect change in helper structure

--- scripts_optimisation/Geospatial_optimisation_deaths.py
+++ scripts_optimisation/Geospatial_optimisation_deaths.py
@@ -9,16 +9,6 @@ moduleDir = os.path.join(os.path.dirname(__file__), '..')
 sys.path.append(moduleDir)
 import optimisation
 
-timestep = 1./12. 
-numsteps = 180
-ages = ["<1 month", "1-5 months", "6-11 months", "12-23 months", "24-59 months"]
-birthOutcomes = ["Pre-term SGA", "Pre-term AGA", "Term SGA", "Term AGA"]
-wastingList = ["normal", "mild", "moderate", "high"]
-stuntingList = ["normal", "mild", "moderate", "high"]
-breastfeedingList = ["exclusive", "predominant", "partial", "none"]
-keyList = [ages, birthOutcomes, wastingList, stuntingList, breastfeedingList]
-ageGroupSpans = [1., 5., 6., 12., 36.] # number of months in each age group
-agingRateList = [1./1., 1./5., 1./6., 1./12., 1./36.] # fraction of people aging out per MONTH (WARNING use ageSpans to define this)
 numModelSteps = 180
 MCSampleSize = 25
 optimise = 'deaths'
@@ -34,7 +24,7 @@ spreadsheetList = [spreadsheet0, spreadsheet1, spreadsheet2, spreadsheet3, sprea
 
 for i in range(0, len(spreadsheetList)):
     spreadsheet = spreadsheetList[i]
-    thisOptimisation = optimisation.Optimisation(spreadsheet, timestep, numModelSteps, ages, birthOutcomes, wastingList, stuntingList, breastfeedingList, ageGroupSpans, agingRateList)
+    thisOptimisation = optimisation.Optimisation(spreadsheet, numModelSteps)
     filename = 'Bangladesh_geospatial_deaths_region_'+str(i)
     thisOptimisation.performSingleOptimisation(optimise, MCSampleSize, filename)
 
