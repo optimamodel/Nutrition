346fae8b1bd912b13f8e739ac5320fc1cb8db63f
“DrMud” <madhura.killedar.astro@gmail.com>
2016-07-12 18:53:57 +1000

    add geospatial read, reformat, and write csv

--- scripts_plotting/readPlot_cascade.py
+++ scripts_plotting/readPlot_cascade.py
@@ -2,15 +2,19 @@
 """
 Created on Tue Jul 12 2016
 
-@author: madhurakilledar
+@author: madhurakilledar and kelseygrantham
 """
 import pickle
-multiples = [0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0, 4.0] 
+cascade = [0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0, 4.0] 
 
 country = 'Bangladesh'
 root = '../../Results2016Jun/%s'%(country)
 
 
+# convert 
+# from dictionary of list of dictionaries of...arrays?
+# to list of lists
+# and a header
 def reformat_results(results):
     from numpy import array
     increments = results.keys()
@@ -31,29 +35,51 @@ def reformat_results(results):
 
 # NATIONAL
 version = 'v5'
-cascade = {}
+national = {}
 for outcome in ['deaths','stunting']:
-    cascade[outcome] = {}
-    for multiple in multiples:
+    national[outcome] = {}
+    for multiple in cascade:
         filename = '%s/%s/%s/%s_cascade_%s_%s_%s.pkl'%(root, outcome, version, country, outcome, version, str(multiple))
         infile = open(filename, 'rb')
         result = pickle.load(infile)
-        cascade[outcome][multiple] = result
+        national[outcome][multiple] = result
         infile.close()
 
-    prognames, rows = reformat_results(cascade[outcome])
+    prognames, rows = reformat_results(national[outcome])
 
     import csv
-    outfilename = '%s/cascade_min_%s.csv'%(root, outcome)
+    outfilename = '%s/national_cascade_min_%s.csv'%(root, outcome)
     with open(outfilename, "wb") as f:
         writer = csv.writer(f)
         writer.writerow(prognames)
         writer.writerows(rows)
 
 
+
 # GEOSPATIAL
+version = 'v3'
+numRegions = 7
+geospatial = {}
+for outcome in ['deaths','stunting']:
+    geospatial[outcome] = {}
+    for iReg in range(numRegions):
+        region = 'region%s'%(iReg)
+        geospatial[outcome][region] = {}
+        for multiple in cascade:
+            filename = '%s/%s/geospatial/%s/%s_cascade_%s_%s.pkl'%(root, outcome, version, region, outcome, str(multiple))
+            infile = open(filename, 'rb')
+            result = pickle.load(infile)
+            geospatial[outcome][region][multiple] = result
+            infile.close()
 
+        prognames, rows = reformat_results(geospatial[outcome][region])
 
+        import csv
+        outfilename = '%s/%s_cascade_min_%s.csv'%(root, region, outcome)
+        with open(outfilename, "wb") as f:
+            writer = csv.writer(f)
+            writer.writerow(prognames)
+            writer.writerows(rows)
 
 
 
