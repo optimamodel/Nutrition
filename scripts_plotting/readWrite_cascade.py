10f2006e0217f6e9a7c4f35c28c84abfc52853f2
“DrMud” <madhura.killedar.astro@gmail.com>
2016-07-15 16:44:34 +1000

    NEW read pkl cascade results, separate codes for writing csv and plotting graphs (draft)

--- scripts_plotting/readWrite_cascade.py
+++ scripts_plotting/readWrite_cascade.py
@@ -36,17 +36,23 @@ def reformat_results(results):
 # NATIONAL
 version = 'v5'
 national = {}
-for multiple in cascade:
-    national[multiple] = {}
-    for outcome in ['deaths','stunting']:
+for outcome in ['deaths','stunting']:
+    national[outcome] = {}
+    for multiple in cascade:
         filename = '%s/%s/%s/%s_cascade_%s_%s_%s.pkl'%(root, outcome, version, country, outcome, version, str(multiple))
         infile = open(filename, 'rb')
         result = pickle.load(infile)
-        national[multiple][outcome] = result
+        national[outcome][multiple] = result
         infile.close()
 
-#    prognames, rows = reformat_results(national[outcome])
+    prognames, rows = reformat_results(national[outcome])
 
+    import csv
+    outfilename = '%s/national_cascade_min_%s.csv'%(root, outcome)
+    with open(outfilename, "wb") as f:
+        writer = csv.writer(f)
+        writer.writerow(prognames)
+        writer.writerows(rows)
 
 
 
@@ -54,17 +60,23 @@ for multiple in cascade:
 version = 'v3'
 numRegions = 7
 geospatial = {}
-for iReg in range(numRegions):
-    region = 'region%s'%(iReg)
-    geospatial[region] = {}
-    for multiple in cascade:
-        geospatial[region][multiple] = {}
-        for outcome in ['deaths','stunting']:
+for outcome in ['deaths','stunting']:
+    geospatial[outcome] = {}
+    for iReg in range(numRegions):
+        region = 'region%s'%(iReg)
+        geospatial[outcome][region] = {}
+        for multiple in cascade:
             filename = '%s/%s/geospatial/%s/%s_cascade_%s_%s.pkl'%(root, outcome, version, region, outcome, str(multiple))
             infile = open(filename, 'rb')
             result = pickle.load(infile)
-            geospatial[region][multiple][outcome] = result
+            geospatial[outcome][region][multiple] = result
             infile.close()
 
-#        prognames, rows = reformat_results(geospatial[outcome][region])
+        prognames, rows = reformat_results(geospatial[outcome][region])
 
+        import csv
+        outfilename = '%s/%s_cascade_min_%s.csv'%(root, region, outcome)
+        with open(outfilename, "wb") as f:
+            writer = csv.writer(f)
+            writer.writerow(prognames)
+            writer.writerows(rows)
