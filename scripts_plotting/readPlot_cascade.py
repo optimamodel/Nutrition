f141205970ef10c076fcefed33d89c9d9bf9f5c9
“DrMud” <madhura.killedar.astro@gmail.com>
2016-07-20 15:58:13 +1000

    make cascade plot assuming plotting.py in root folder; remove unnecessary function

--- scripts_plotting/readPlot_cascade.py
+++ scripts_plotting/readPlot_cascade.py
@@ -5,48 +5,30 @@ Created on Tue Jul 12 2016
 @author: madhurakilledar and kelseygrantham
 """
 import pickle
+import os, sys
+moduleDir = os.path.join(os.path.dirname(__file__), '..')
+sys.path.append(moduleDir)
+from plotting import plotalloccascade
+
 cascadeMultiples = [0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0, 4.0] 
 
 country = 'Bangladesh'
 root = '../../Results2016Jun/%s'%(country)
 
 
-# convert 
-# from dictionary of list of dictionaries of...arrays?
-# to list of lists
-# and a header
-def reformat_results(results):
-    from numpy import array
-    increments = results.keys()
-    spendingsets = results.values()
-    rows = []
-    for i in range(len(increments)):
-        chunk = spendingsets[i]
-        values = array(chunk.values()).tolist()
-        valarray = [item for sublist in values for item in sublist]
-        valarray.insert(0, increments[i])
-        rows.append(valarray)
-    rows.sort()
-    prognames = spendingsets[0].keys()
-    prognames.insert(0, 'Multiple')
-    return prognames, rows
-
-
-
 # NATIONAL
 version = 'v5'
 national = {}
-for multiple in cascadeMultiples:
-    national[multiple] = {}
-    for outcome in ['deaths','stunting']:
-        filename = '%s/%s/%s/%s_cascade_%s_%s_%s.pkl'%(root, outcome, version, country, outcome, version, str(multiple))
+for objective in ['deaths','stunting']:
+    national[objective] = {}
+    for multiple in cascadeMultiples:
+        filename = '%s/%s/%s/%s_cascade_%s_%s_%s.pkl'%(root, objective, version, country, objective, version, str(multiple))
         infile = open(filename, 'rb')
-        result = pickle.load(infile)
-        national[multiple][outcome] = result
+        allocation = pickle.load(infile)
+        national[objective][multiple] = allocation
         infile.close()
-
-#    prognames, rows = reformat_results(national[outcome])
-
+    # plot
+    plotalloccascade(national[objective])
 
 
 
@@ -57,14 +39,13 @@ geospatial = {}
 for iReg in range(numRegions):
     region = 'region%s'%(iReg)
     geospatial[region] = {}
-    for multiple in cascadeMultiples:
-        geospatial[region][multiple] = {}
-        for outcome in ['deaths','stunting']:
-            filename = '%s/%s/geospatial/%s/%s_cascade_%s_%s.pkl'%(root, outcome, version, region, outcome, str(multiple))
+    for objective in ['deaths','stunting']:
+        geospatial[region][objective] = {}
+        for multiple in cascadeMultiples:
+            filename = '%s/%s/geospatial/%s/%s_cascade_%s_%s.pkl'%(root, objective, version, region, objective, str(multiple))
             infile = open(filename, 'rb')
-            result = pickle.load(infile)
-            geospatial[region][multiple][outcome] = result
+            allocation = pickle.load(infile)
+            geospatial[region][objective][multiple] = allocation
             infile.close()
-
-#        prognames, rows = reformat_results(geospatial[outcome][region])
-
+        # plot
+        plotalloccascade(geospatial[region][objective])
