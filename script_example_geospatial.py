8b3429bda90adca3cd5798e19e89da27474aa85a
Ruth Pearson <ruth.pearson8@gmail.com>
2016-07-25 11:24:51 +1000

    tidied up example geospatial script for use as reference on how to use geospatial class

--- script_example_geospatial.py
+++ script_example_geospatial.py
@@ -3,13 +3,16 @@
 Created on Mon Jul 11 17:54:47 2016
 
 @author: ruth
+
+This is an example script of how to use the geospatial class to generate and harvest results.
+Use as a reference, do not edit.
 """
 
 import optimisation
 
 numModelSteps = 180
 MCSampleSize = 25
-geoMCSampleSize = 1
+geoMCSampleSize = 25
 cascadeValues = [0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0, 4.0]  
 optimise = 'deaths'
 spreadsheet0 = 'input_spreadsheets/Bangladesh/subregionSpreadsheets/Barisal.xlsx'
@@ -19,25 +22,32 @@ spreadsheet3 = 'input_spreadsheets/Bangladesh/subregionSpreadsheets/Khulna.xlsx'
 spreadsheet4 = 'input_spreadsheets/Bangladesh/subregionSpreadsheets/Rajshahi.xlsx'
 spreadsheet5 = 'input_spreadsheets/Bangladesh/subregionSpreadsheets/Rangpur.xlsx'
 spreadsheet6 = 'input_spreadsheets/Bangladesh/subregionSpreadsheets/Sylhet.xlsx'
-spreadsheetList = [spreadsheet0, spreadsheet1] #, spreadsheet2, spreadsheet3, spreadsheet4, spreadsheet5, spreadsheet6]
+spreadsheetList = [spreadsheet0, spreadsheet1, spreadsheet2, spreadsheet3, spreadsheet4, spreadsheet5, spreadsheet6]
 
-regionNameList = ['Barisal', 'Chittagong'] #, 'Dhaka', 'Khulna', 'Rajshahi', 'Rangpur', 'Sylhet']
-#regionNameList = ['region0', 'region1', 'region2', 'region3', 'region4', 'region5', 'region6']
+regionNameList = ['Barisal', 'Chittagong', 'Dhaka', 'Khulna', 'Rajshahi', 'Rangpur', 'Sylhet']
+
+# this is the location of the optimisation results per region (to be generated or harvested)
 resultsFileStem = 'ResultsExampleParallel/deaths/geospatial/'
+
+# this is the location of the results from the geospatial analysis
 GAresultsFileStem = 'ResultsExampleParallel/deaths/geospatial/GAResult'
 
-#geospatialOptimisation = optimisation.GeospatialOptimisation(spreadsheetList, regionNameList, numModelSteps, cascadeValues, optimise, resultsFileStem)
-#geospatialOptimisation.generateAllRegionsBOC()
-#geospatialOptimisation.performGeospatialOptimisation(geoMCSampleSize, MCSampleSize, GAresultsFileStem)
-#geospatialOptimisation.generateResultsForGeospatialCascades(MCSampleSize)
-
-geospatialOptimisation = optimisation.GeospatialOptimisation(spreadsheetList, regionNameList, numModelSteps, cascadeValues, optimise, resultsFileStem)
+# check athena and then specify how many cores you are going to use (this translates into the number of jobs as we assume 1 core per job)
 nCores = 4
+
+# instantiate a geospatial object
+geospatialOptimisation = optimisation.GeospatialOptimisation(spreadsheetList, regionNameList, numModelSteps, cascadeValues, optimise, resultsFileStem)
+
+# use it to genarate geospatial cascades (these will be stored in the resultsFileStem location)
 geospatialOptimisation.generateParallelResultsForGeospatialCascades(nCores, MCSampleSize)
 
-#numCores = 2
-#optimisation1 = optimisation.Optimisation(spreadsheet0, numModelSteps)
-#optimisation1.performParallelCascadeOptimisation(optimise, MCSampleSize, resultsFileStem, cascadeValues, numCores)
+# now harvest those results to perform the geospatial optimisation
+geospatialOptimisation.performGeospatialOptimisation(geoMCSampleSize, MCSampleSize, GAresultsFileStem)
+
+
+
+
+
 
 
 
