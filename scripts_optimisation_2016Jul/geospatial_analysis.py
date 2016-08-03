f50bead06aaea1f28dd02b1c383b861b8ee36ce3
Ruth Pearson <ruth.pearson8@gmail.com>
2016-08-03 14:11:45 +1000

    updated for geospatial DALYs run

--- scripts_optimisation_2016Jul/geospatial_analysis.py
+++ scripts_optimisation_2016Jul/geospatial_analysis.py
@@ -10,14 +10,14 @@ moduleDir = os.path.join(os.path.dirname(__file__), '..')
 sys.path.append(moduleDir)
 import optimisation
 
-optimiseList = ['deaths', 'stunting']
+optimiseList = ['DALYs']  #['deaths', 'stunting']
 
 numModelSteps = 180
 MCSampleSize = 25
 geoMCSampleSize = 25
 cascadeValues = [0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0, 4.0]  
 regionNameList = ['Barisal', 'Chittagong', 'Dhaka', 'Khulna', 'Rajshahi', 'Rangpur', 'Sylhet']
-spreadsheetFileStem = '../input_spreadsheets/Bangladesh/2016Jul26/subregionSpreadsheets/'
+spreadsheetFileStem = '../input_spreadsheets/Bangladesh/2016Aug02/subregionSpreadsheets/'
 spreadsheetList = []
 for regionName in regionNameList:
     spreadsheet = spreadsheetFileStem + regionName + '.xlsx'
@@ -30,7 +30,7 @@ numCores = 7 # need twice this many available becasue optimising for
 for optimise in optimiseList:
     print 'running GA for:  ', optimise
     
-    resultsFileStem = '../Results2016Jul26/'+optimise+'/geospatial/'
+    resultsFileStem = '../Results2016Aug02/'+optimise+'/geospatial/'
     GAFile = 'GA'    
     
     geospatialOptimisation = optimisation.GeospatialOptimisation(spreadsheetList, regionNameList, numModelSteps, cascadeValues, optimise, resultsFileStem)
---
+++ scripts_optimisation_2016Jul/national_analysis.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  3 13:03:54 2016
+
+@author: ruth
+"""
+
+import os, sys
+moduleDir = os.path.join(os.path.dirname(__file__), '..')
+sys.path.append(moduleDir)
+import optimisation
+
+optimise = 'stunting'
+
+dataSpreadsheetName = '../input_spreadsheets/Bangladesh/2016Aug02/InputForCode_Bangladesh.xlsx'
+numModelSteps = 180
+MCSampleSize = 25
+cascadeValues = [0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0, 4.0]  
+resultsFileStem = '../Results2016Aug02/'+optimise+'/national/Bangladesh'
+
+thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem)
+#thisOptimisation.plotReallocation()
+thisOptimisation.plotTimeSeries()
