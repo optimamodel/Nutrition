e42ceaf1b9319a8f5bd03b11dae5f1514143d15b
Ruth Pearson <ruth.pearson8@gmail.com>
2016-08-05 10:58:06 +1000

    modified scripts for run

--- scripts_optimisation_2016Jul/geospatial_DALYs.py
+++ scripts_optimisation_2016Jul/geospatial_DALYs.py
@@ -11,11 +11,11 @@ sys.path.append(moduleDir)
 import optimisation
 
 optimise = 'DALYs'
-nCores = 49
+nCores = 7
 
 numModelSteps = 180
 MCSampleSize = 25
-cascadeValues = [0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0] #, 4.0]  
+cascadeValues = ['extreme'] #[0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0, 'extreme'] #, 4.0]  
 regionNameList = ['Barisal', 'Chittagong', 'Dhaka', 'Khulna', 'Rajshahi', 'Rangpur', 'Sylhet']
 spreadsheetFileStem = '../input_spreadsheets/Bangladesh/2016Aug02/subregionSpreadsheets/'
 spreadsheetList = []
@@ -26,7 +26,4 @@ for regionName in regionNameList:
 resultsFileStem = '../Results2016Aug02/'+optimise+'/geospatial/'
 
 geospatialOptimisation = optimisation.GeospatialOptimisation(spreadsheetList, regionNameList, numModelSteps, cascadeValues, optimise, resultsFileStem)
-#geospatialOptimisation.generateParallelResultsForGeospatialCascades(nCores, MCSampleSize)
-
-numCores = 7
-geospatialOptimisation.generateParallelExtremeGeospatialCascadePoints(numCores, MCSampleSize)
+geospatialOptimisation.generateParallelResultsForGeospatialCascades(nCores, MCSampleSize)
