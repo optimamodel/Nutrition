285430989e58c6ad85187c731c1b207c0d77c759
Ruth Pearson <ruth.pearson8@gmail.com>
2016-08-11 17:58:17 +1000

    changed script to generate required csvs

--- script_results_harvest_Aug.py
+++ script_results_harvest_Aug.py
@@ -5,55 +5,68 @@ Created on Fri Aug  5 15:15:58 2016
 @author: ruth
 """
 import optimisation
-optimise = 'DALYs'
+optimiseList = ['deaths', 'stunting'] #['DALYs'] #, 'deaths', 'stunting']
 numModelSteps = 180
 MCSampleSize = 25
-stem = 'resultsHarvest2016Aug05'
 
-## NATIONAL
-#dataSpreadsheetName = '../input_spreadsheets/Bangladesh/2016Aug02/InputForCode_Bangladesh.xlsx'
-#resultsFileStem = '../Results2016Aug02/'+optimise+'/national/Bangladesh'
-#cascadeValues = [0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0, 4.0]  
-#
-#thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem)
-#thisOptimisation.plotReallocation()
-#thisOptimisation.plotTimeSeries()
+# NATIONAL
+dataSpreadsheetName = 'input_spreadsheets/Bangladesh/2016Aug02/InputForCode_Bangladesh.xlsx'
+cascadeValues = [0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0, 4.0]  
+# DALYs: bar graph, all 3 time series when optimising for DALYs
+optimise = 'DALYs'
+resultsFileStem = 'Results2016Aug10/'+optimise+'/national/Bangladesh'
+thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem)
+thisOptimisation.outputCurrentSpendingToCSV() 
+thisOptimisation.outputTimeSeriesToCSV('DALYs')
+thisOptimisation.outputTimeSeriesToCSV('deaths')
+thisOptimisation.outputTimeSeriesToCSV('stunting')
+# all 3 cascades and outcomes
+for optimise in optimiseList:
+    resultsFileStem = 'Results2016Aug10/'+optimise+'/national/Bangladesh'
+    thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem)
+    thisOptimisation.outputCascadeAndOutcomeToCSV(cascadeValues, optimise)
 
 
-# GEOSPATIAL 
-extraMoney = 10000000
-cascadeValues = [0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0, 'extreme'] 
+# GEOSPATIAL
+optimise = 'DALYs'
 regionNameList = ['Barisal', 'Chittagong', 'Dhaka', 'Khulna', 'Rajshahi', 'Rangpur', 'Sylhet']
 spreadsheetFileStem = 'input_spreadsheets/Bangladesh/2016Aug02/subregionSpreadsheets/'
 spreadsheetList = []
 for regionName in regionNameList:
     spreadsheet = spreadsheetFileStem + regionName + '.xlsx'
     spreadsheetList.append(spreadsheet)
-    
-resultsFileStem = 'Results2016Aug02/'+optimise+'/geospatial/'
 
+
+# GEOSPATIAL WITHOUT EXTRA MONEY
+# DALYs: bar graph/region, trade off curves, cascade and outcomes for DALYs    
+cascadeValues = [0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0, 'extreme']  
+GAFile = 'GA'  
+resultsFileStem = 'Results2016Aug10/'+optimise+'/geospatial/'
 geospatialOptimisation = optimisation.GeospatialOptimisation(spreadsheetList, regionNameList, numModelSteps, cascadeValues, optimise, resultsFileStem)
-#geospatialOptimisation.outputRegionalBOCsFile(stem+'_regionalBOCS_'+optimise+'.pkl')
-#geospatialOptimisation.outputTradeOffCurves(stem+'_tradeOffCurves_'+optimise+'.pkl')
-#geospatialOptimisation.plotRegionalBOCs()
-#geospatialOptimisation.plotTradeOffCurves()
-#geospatialOptimisation.plotReallocationByRegion()
+geospatialOptimisation.outputRegionalCurrentSpendingToCSV()
+geospatialOptimisation.outputRegionalPostGAOptimisedSpendingToCSV(GAFile)
+geospatialOptimisation.outputTradeOffCurves()
+geospatialOptimisation.outputRegionalCascadesAndOutcomeToCSV('DALYs')
 
-#GAFile1 = 'GA'
-#GAFile2 = 'GA_extra_'+str(extraMoney) 
-#geospatialOptimisation.plotPostGAReallocationByRegion(GAFile1)
-#geospatialOptimisation.plotTimeSeriesPostGAReallocationByRegion(GAFile1)
-#geospatialOptimisation.plotPostGAReallocationByRegion(GAFile2)
-#geospatialOptimisation.plotTimeSeriesPostGAReallocationByRegion(GAFile2)
 
-#import pickle
-#filename = stem+'_regionalBOCS_'+optimise+'.pkl'
-#infile = open(filename, 'rb')
-#this = pickle.load(infile)
-#infile.close()
-#
-#geospatialOptimisation.regionalBOCs = this
-geospatialOptimisation.plotTradeOffCurves()
+# GEOSPATIAL EXTRA MONEY
+extraMoney = 10000000
+cascadeValuesExtra = [1.0, 1.20, 1.50, 1.80, 2.0, 3.0, 'extreme']
+# DALYs extra money: bar graph/region, trade off curves, cascade and outcomes for DALYs    
+GAFileExtra = 'GA_fixedProg_extra_'+str(extraMoney)   
+resultsFileStem = 'Results2016Aug10fixedProgCosts/'+optimise+'/geospatial/'
+geospatialOptimisation = optimisation.GeospatialOptimisation(spreadsheetList, regionNameList, numModelSteps, cascadeValuesExtra, optimise, resultsFileStem)
+geospatialOptimisation.outputRegionalCurrentSpendingToCSV()
+geospatialOptimisation.outputRegionalPostGAOptimisedSpendingToCSV(GAFileExtra)
+geospatialOptimisation.outputTradeOffCurves()
+geospatialOptimisation.outputRegionalCascadesAndOutcomeToCSV('DALYs')
+
+
+
+
+
+
+
 
 
 
