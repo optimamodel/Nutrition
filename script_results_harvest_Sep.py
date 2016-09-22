c602726e7a802033547f0857ae2caa87baad83d8
Ruth Pearson <ruth.pearson8@gmail.com>
2016-09-22 10:37:58 +1000

    script to harvest latest results

---
+++ script_results_harvest_Sep.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 22 10:15:43 2016
+
+@author: ruth
+"""
+
+import optimisation
+outcomeOfInterestList = ['deaths', 'stunting', 'DALYs'] 
+numModelSteps = 180
+MCSampleSize = 25
+
+# NATIONAL
+dataSpreadsheetName = 'input_spreadsheets/Bangladesh/2016Sept12/InputForCode_Bangladesh.xlsx'
+cascadeValues = [0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0, 4.0]  
+optimise = 'stunting'
+resultsFileStem = 'Results2016Sep21/'+optimise+'/national/Bangladesh'
+# stunting: bar graph, all 3 time series when optimising for stunting
+thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem)
+thisOptimisation.outputCurrentSpendingToCSV() 
+thisOptimisation.outputTimeSeriesToCSV('DALYs')
+thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem) #this repitition is necessary
+thisOptimisation.outputTimeSeriesToCSV('deaths')
+thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem) #this repitition is necessary
+thisOptimisation.outputTimeSeriesToCSV('stunting')
+# all 3 outcomes for stunting cascade
+thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem) # this one might not be
+for outcomeOfInterest in outcomeOfInterestList:
+    thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem)
+    thisOptimisation.outputCascadeAndOutcomeToCSV(cascadeValues, outcomeOfInterest)
+    
+# cascades for deaths and DALYs
+thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, 'deaths', resultsFileStem)
+thisOptimisation.outputCascadeAndOutcomeToCSV(cascadeValues, 'deaths') 
+thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, 'DALYs', resultsFileStem)
+thisOptimisation.outputCascadeAndOutcomeToCSV(cascadeValues, 'DALYs')    
+    
+
+### GEOSPATIAL
+#optimise = 'DALYs'
+#regionNameList = ['Barisal', 'Chittagong', 'Dhaka', 'Khulna', 'Rajshahi', 'Rangpur', 'Sylhet']
+#spreadsheetFileStem = 'input_spreadsheets/Bangladesh/2016Sept12/subregionSpreadsheets/'
+#spreadsheetList = []
+#for regionName in regionNameList:
+#    spreadsheet = spreadsheetFileStem + regionName + '.xlsx'
+#    spreadsheetList.append(spreadsheet)
+#
+## GEOSPATIAL WITHOUT EXTRA MONEY
+## DALYs: bar graph/region, trade off curves, cascade and outcomes for DALYs    
+#cascadeValues = [0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0, 'extreme']  
+#GAFile = 'GA'  
+#resultsFileStem = 'Results2016Sep21/'+optimise+'/geospatial/'
+#geospatialOptimisation = optimisation.GeospatialOptimisation(spreadsheetList, regionNameList, numModelSteps, cascadeValues, optimise, resultsFileStem)
+#geospatialOptimisation.outputRegionalCurrentSpendingToCSV()
+#geospatialOptimisation.outputRegionalPostGAOptimisedSpendingToCSV(GAFile)
+#geospatialOptimisation.outputTradeOffCurves()
+#geospatialOptimisation.outputRegionalCascadesAndOutcomeToCSV('DALYs')
+#geospatialOptimisation.outputRegionalTimeSeriesToCSV('DALYs')
+#
+## GEOSPATIAL EXTRA MONEY
+#extraMoney = 10000000
+#cascadeValuesExtra = [1.0, 1.20, 1.50, 1.80, 2.0, 3.0, 'extreme']
+## DALYs extra money: bar graph/region, trade off curves, cascade and outcomes for DALYs    
+#GAFileExtra = 'GA_fixedProg_extra_'+str(extraMoney)   
+#resultsFileStem = 'Results2016Sep21fixedProgCosts/'+optimise+'/geospatial/'
+#geospatialOptimisation = optimisation.GeospatialOptimisation(spreadsheetList, regionNameList, numModelSteps, cascadeValuesExtra, optimise, resultsFileStem)
+#geospatialOptimisation.outputRegionalCurrentSpendingToCSV()
+#geospatialOptimisation.outputRegionalPostGAOptimisedSpendingToCSV(GAFileExtra)
+#geospatialOptimisation.outputTradeOffCurves()
+#geospatialOptimisation.outputRegionalCascadesAndOutcomeToCSV('DALYs')
+#geospatialOptimisation.outputRegionalTimeSeriesToCSV('DALYs')
