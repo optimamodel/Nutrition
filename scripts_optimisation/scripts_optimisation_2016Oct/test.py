91492bb6a4f69b3b0ccb1381f89a6f490ceea248
Ruth Pearson <ruth.pearson8@gmail.com>
2016-10-27 14:12:46 +1100

    added test script to check athena directory write permission sucess

---
+++ scripts_optimisation/scripts_optimisation_2016Oct/test.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 27 14:10:32 2016
+
+@author: ruth
+"""
+
+import os, sys
+moduleDir = os.path.join(os.path.dirname(__file__), '../..')
+sys.path.append(moduleDir)
+import optimisation
+
+
+rootpath = '../..'
+
+country = 'Bangladesh'
+date = '2016Oct'
+
+spreadsheet = rootpath+'/input_spreadsheets/'+country+'/'+date+'/InputForCode_'+country+'.xlsx'
+
+numModelSteps = 12
+MCSampleSize = 1
+cascadeValues = [7.0]  
+haveFixedProgCosts = False
+numCores = 8
+
+for optimise in ['stunting']:
+
+    resultsFileStem = rootpath+'/Results/'+date+'/'+optimise+'/national/'+country+'_test'
+
+    thisOptimisation = optimisation.Optimisation(spreadsheet, numModelSteps, optimise, resultsFileStem)
+    thisOptimisation.performParallelCascadeOptimisation(MCSampleSize, cascadeValues, numCores, haveFixedProgCosts)
