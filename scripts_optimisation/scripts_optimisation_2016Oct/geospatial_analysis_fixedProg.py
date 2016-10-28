dabf0d4d4565dccf644ed7dba0c39b5ebb123d9c
“DrMud” <madhura.killedar.astro@gmail.com>
2016-10-28 16:56:50 +1100

    update optimiseList; add warning; define and update resultsPath

--- scripts_optimisation/scripts_optimisation_2016Oct/geospatial_analysis_fixedProg.py
+++ scripts_optimisation/scripts_optimisation_2016Oct/geospatial_analysis_fixedProg.py
@@ -12,20 +12,23 @@ moduleDir = os.path.join(os.path.dirname(__file__), rootpath)
 sys.path.append(moduleDir)
 import optimisation
 
-
 country = 'Bangladesh'
 date = '2016Oct'
 
-optimiseList = ['stunting']  #['DALYs', 'stunting', 'deaths']
+optimiseList = ['stunting','deaths'] #['DALYs', 'stunting', 'deaths']
+
 extraMoney = 10000000
 haveFixedProgCosts = True
 
-numModelSteps = 180
-MCSampleSize = 25
+rerunMCSampleSize = 25
 geoMCSampleSize = 25
+# WARNING: these must match values used in the other geospatial scripts
+numModelSteps = 180
 cascadeValues = [1.0, 1.1, 1.25, 1.5, 1.7, 2.0, 'extreme'] 
-regionNameList = ['Barisal', 'Chittagong', 'Dhaka', 'Khulna', 'Rajshahi', 'Rangpur', 'Sylhet']
 
+resultsPath = '/home/ruthpearson/Nutrition/Results/'
+
+regionNameList = ['Barisal', 'Chittagong', 'Dhaka', 'Khulna', 'Rajshahi', 'Rangpur', 'Sylhet']
 spreadsheetFileStem = rootpath+'/input_spreadsheets/'+country+'/'+date+'/subregionSpreadsheets/'
 spreadsheetList = []
 for regionName in regionNameList:
@@ -37,10 +40,10 @@ numCores = 8 # need this number times the number of outcomes you're optimising f
 for optimise in optimiseList:
     print 'running GA for:  ', optimise
     
-    resultsFileStem = rootpath+'/Results/'+date+'fixedProgCosts/'+optimise+'/geospatial/'
+    resultsFileStem = resultsPath+'/Results/'+date+'fixedProgCosts/'+optimise+'/geospatial/'
     GAFile = 'GA_fixedProg_extra_'+str(extraMoney)    
     
     geospatialOptimisation = optimisation.GeospatialOptimisation(spreadsheetList, regionNameList, numModelSteps, cascadeValues, optimise, resultsFileStem)
-    geospatialOptimisation.performParallelGeospatialOptimisationExtraMoney(geoMCSampleSize, MCSampleSize, GAFile, numCores, extraMoney, haveFixedProgCosts)
+    geospatialOptimisation.performParallelGeospatialOptimisationExtraMoney(geoMCSampleSize, rerunMCSampleSize, GAFile, numCores, extraMoney, haveFixedProgCosts)
 
     
