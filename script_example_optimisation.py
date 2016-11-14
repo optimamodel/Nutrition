4068c52faa6942511eccabc40c0fff2746957e30
Ruth Pearson <ruth.pearson8@gmail.com>
2016-11-14 14:07:22 +1100

    updates and edits

--- script_example_optimisation.py
+++ script_example_optimisation.py
@@ -9,29 +9,34 @@ Use as a reference, do not edit.
 """
 
 import optimisation
+country = 'Bangladesh'
+date = '2016Oct'
 
-dataSpreadsheetName = '../input_spreadsheets/Bangladesh/2016Jul26/InputForCode_Bangladesh.xlsx'
-numModelSteps = 24 #180
+dataSpreadsheetName = '/input_spreadsheets/'+country+'/'+date+'/InputForCode_'+country+'.xlsx'
+numModelSteps = 180
 MCSampleSize = 25
 cascadeValues = [0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0, 4.0]
 optimise = 'stunting'  
 
 # this is the location of the optimisation results per region (to be generated or harvested)
-resultsFileStem = 'ResultsExample/'+optimise+'/national/Bangladesh'
+resultsFileStem = '/Results/'+date+'/'+optimise+'/national/'+country
 
 # check athena and then specify how many cores you are going to use (this translates into the number of jobs as we assume 1 core per job)
 # this is only needed if you plan to run in parallel
 numCores = 8
 
+# this specifies if the current coverage of programs is fixed.  If True, only money above 100% of current spending will be optimised
+haveFixedProgCosts = False
+
 # instantiate an optimisation object
 thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem)
 
-# use it to genarate geospatial cascades if they're not already there (these will be stored in the resultsFileStem location)
+# use it to genarate the cascade files if they're not already there (these will be stored in the resultsFileStem location)
 # this example generates the cascade in parallel (e.g. on athena)
-thisOptimisation.performParallelCascadeOptimisation(MCSampleSize, cascadeValues, numCores)
+thisOptimisation.performParallelCascadeOptimisation(MCSampleSize, cascadeValues, numCores, haveFixedProgCosts)
 
 # this example generates the cascade but not in parallel (eg on your laptop)
-thisOptimisation.performCascadeOptimisation(MCSampleSize, cascadeValues, numCores)
+thisOptimisation.performCascadeOptimisation(MCSampleSize, cascadeValues, numCores, haveFixedProgCosts)
 
 # perform one optimisation of the curent budget
 # the output file will be (resultsFileStem):
@@ -52,7 +57,18 @@ thisOptimisation.performSingleOptimisationForGivenTotalBudget(MCSampleSize, tota
 # output can be used to access time series qualities of the model run
 modelOutput = thisOptimisation.oneModelRunWithOutput(totalBudget)
 
+# using functions to generate output from the .pkl results files (harvesting results)
+# if this is done in a separate script, the optimisation instance is setup as above
 
+# make a csv containing the current spending 
+thisOptimisation.outputCurrentSpendingToCSV() 
+# output time-series to CSV (outcomeOfInterest doesn't have to be the same as optimise)
+outcomeOfInterest = 'deaths'
+thisOptimisation.outputTimeSeriesToCSV(outcomeOfInterest)
+
+# output the spending cascade and the outcome cascade to csv 
+thisOptimisation.outputCascadeAndOutcomeToCSV(cascadeValues, outcomeOfInterest)
+    
 
 
 
