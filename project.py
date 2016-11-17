feb3f34f2e514ca362084e85079609ce64b93c30
Ruth Pearson <ruth.pearson8@gmail.com>
2016-11-17 14:12:14 +1100

    added projectData class and project spreadsheet which gets read

--- project.py
+++ project.py
@@ -30,3 +30,65 @@ class Project:
         self.dataSpreadsheets['national'] = dataSpreadsheetGeospatial    
         
         
+class ProjectData:
+    def __init__(self, projectDataSpreadsheetName):
+        self.projectDataSpreadsheetName = projectDataSpreadsheetName        
+        self.projectName = None        
+        self.projectDescription = None         
+        self.analysisType = None
+        self.spendingConstraints = None
+        self.dataSpreadsheetName = None
+        self.country = None
+        self.date = None
+        self.numYears = None
+        self.optimise = None
+        self.cascade = None
+        self.extraMoney = None
+        self.MCSampleSize = None
+        self.geoMCSampleSize = None
+        self.reRunMCSampleSize = None
+        self.dataFromSpreadsheet = None
+        
+        self.readProjectDataFromSpreadsheet()
+        
+    def readProjectDataFromSpreadsheet(self):    
+        #read from csv         
+        import pandas
+        import data
+        import helper
+        helper = helper.Helper()
+        Location = self.projectDataSpreadsheetName     
+        df = pandas.read_excel(Location, sheetname = 'data')
+        dataList = list(df['data'])
+        df = pandas.read_excel(Location, sheetname = 'data', index_col = 'data')
+        values = {}
+        # if the values aren't nan add them to the dictionary
+        for d in dataList:
+            thisValue = df.loc[d, 'values']
+            checkNull = pandas.isnull(thisValue)
+            if checkNull:
+                values[d] = None
+            else:
+                values[d] = thisValue
+        # put the values into the class variables
+        self.projectName = values['project name']        
+        self.projectDescription = values['project description']         
+        self.analysisType = values['analysis type']
+        self.spendingConstraints = values['spending constraints']
+        self.dataSpreadsheetName = values['data spreadsheet']
+        self.country = values['country']
+        self.date = values['date']
+        self.numYears = values['number of years']
+        self.optimise = values['optimise for']
+        self.cascade = values['cascade multiples']
+        self.extraMoney = values['extra money']
+        self.MCSampleSize = values['MCSampleSize']
+        self.geoMCSampleSize = values['geoMCSampleSize']
+        self.reRunMCSampleSize = values['reRunMCSampleSize']
+        # read the data from data spreasdsheet into data object
+        self.dataFromSpreadsheet = data.readSpreadsheet(self.dataSpreadsheetName, helper.keyList)
+        
+            
+        
+        
+        
