d5ab265bd7681f9d7d68ce42512730933c217587
Ruth Pearson <ruth.pearson8@gmail.com>
2017-04-18 12:04:00 +1000

    made a test scripts for some quick checks that everything is running OK on athena

---
+++ scripts_optimisation/test.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 18 12:01:30 2017
+
+@author: ruth
+"""
+
+import os, sys
+moduleDir = os.path.join(os.path.dirname(__file__), '../..')
+sys.path.append(moduleDir)
+import optimisation
+
+
+rootpath = '../..'
+
+country = 'Bangladesh'
+date = '2017Apr'
+spreadsheetDate = '2016Oct'
+
+spreadsheet = rootpath+'/input_spreadsheets/'+country+'/'+spreadsheetDate+'/InputForCode_'+country+'.xlsx'
+
+numModelSteps = 24
+MCSampleSize = 1
+cascadeValues = [0.25]  
+haveFixedProgCosts = False
+numCores = 8
+
+for optimise in ['deaths']:
+
+    resultsFileStem = rootpath+'/Results/'+date+'/'+optimise+'/national/'+country
+
+    thisOptimisation = optimisation.Optimisation(spreadsheet, numModelSteps, optimise, resultsFileStem)
+    thisOptimisation.performParallelCascadeOptimisation(MCSampleSize, cascadeValues, numCores, haveFixedProgCosts)
