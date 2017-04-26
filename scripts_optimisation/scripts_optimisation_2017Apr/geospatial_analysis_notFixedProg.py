2b97958c954f2907ec90930f5deea23b74b5448e
SamuelHainsworth <samhainsworth@SHainsworthNov16.local>
2017-04-26 13:05:01 +1000

    Created scripts for geospatial optimisation of 'thrive' and analysis

---
+++ scripts_optimisation/scripts_optimisation_2017Apr/geospatial_analysis_notFixedProg.py
@@ -0,0 +1,47 @@
+"""
+Created on Wed April 26 2017
+
+@author: sam
+"""
+
+rootpath = '../..'
+
+import os, sys
+
+moduleDir = os.path.join(os.path.dirname(__file__), rootpath)
+sys.path.append(moduleDir)
+import optimisation
+
+country = 'Bangladesh'
+date = '2017Act'
+spreadsheetDate = '2016Oct'
+
+extraMoney = 0
+haveFixedProgCosts = False
+
+rerunMCSampleSize = 25
+geoMCSampleSize = 25
+# WARNING: these must match values used in the other geospatial scripts
+numModelSteps = 180
+cascadeValues = [1.0, 1.1, 1.25, 1.5, 1.7, 2.0, 'extreme']
+
+resultsPath = '/home/ruthpearson/Nutrition/Results/'
+
+regionNameList = ['Barisal', 'Chittagong', 'Dhaka', 'Khulna', 'Rajshahi', 'Rangpur', 'Sylhet']
+spreadsheetFileStem = rootpath + '/input_spreadsheets/' + country + '/' + spreadsheetDate + '/subregionSpreadsheets/'
+spreadsheetList = []
+for regionName in regionNameList:
+    spreadsheet = spreadsheetFileStem + regionName + '.xlsx'
+    spreadsheetList.append(spreadsheet)
+
+numCores = 8  # need this number times the number of outcomes you're optimising for
+for optimise in ['thrive']:
+    print 'running GA for:  ', optimise
+
+    resultsFileStem = resultsPath + '/' + date + '/' + optimise + '/geospatial/'
+    GAFile = 'GA_fixedProg_extra_' + str(extraMoney)
+
+    geospatialOptimisation = optimisation.GeospatialOptimisation(spreadsheetList, regionNameList, numModelSteps,
+                                                                 cascadeValues, optimise, resultsFileStem)
+    geospatialOptimisation.performParallelGeospatialOptimisationExtraMoney(geoMCSampleSize, rerunMCSampleSize, GAFile,
+                                                                           numCores, extraMoney, haveFixedProgCosts)
