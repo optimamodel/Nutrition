71bf8470b6842eca89d4e1494e7e2392f8589a37
Ruth Pearson <ruth.pearson8@gmail.com>
2017-05-01 11:00:47 +1000

    added coverage stuff to national harvest script

--- scripts_results_harvest/harvest_2017Apr.py
+++ scripts_results_harvest/harvest_2017Apr.py
@@ -18,36 +18,60 @@ numModelSteps = 180
 
 # NATIONAL
 dataSpreadsheetName = rootpath+'/input_spreadsheets/'+country+'/'+spreadsheetDate+'/InputForCode_'+country+'.xlsx'
-outcomeOfInterestList = ['stunting','stunting prev', 'thrive']
+outcomeOfInterestList = ['thrive', 'deaths']
 cascadeValues = [0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0, 4.0]  
 
 
 optimise = 'thrive'
 resultsFileStem = rootpath+'/Results/'+date+'/'+optimise+'/national/'+country
-# all 3 time series when optimising for thrive
+# both time series when optimising for thrive
 thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem)
 thisOptimisation.outputCurrentSpendingToCSV() 
 thisOptimisation.outputTimeSeriesToCSV('thrive')
 thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem) #this repitition is necessary
-thisOptimisation.outputTimeSeriesToCSV('stunting prev')
-thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem) #this repitition is necessary
-thisOptimisation.outputTimeSeriesToCSV('stunting')
-# all 3 outcomes for thrive cascade
+thisOptimisation.outputTimeSeriesToCSV('deaths')
+# both outcomes for thrive cascade
 thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem) # this one might not be
 for outcomeOfInterest in outcomeOfInterestList:
     thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem)
     thisOptimisation.outputCascadeAndOutcomeToCSV(cascadeValues, outcomeOfInterest)
     
-## cascades for deaths and DALYs
-#optimise = 'deaths'
-#resultsFileStem = rootpath+'/Results/'+date+'/'+optimise+'/national/'+country
-#thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem)
-#thisOptimisation.outputCascadeAndOutcomeToCSV(cascadeValues, 'deaths') 
-#thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem)
-#thisOptimisation.outputCascadeAndOutcomeToCSV(cascadeValues, 'stunting') 
-#optimise = 'DALYs'
-#resultsFileStem = rootpath+'/Results/'+date+'/'+optimise+'/national/'+country
-#thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem)
-#thisOptimisation.outputCascadeAndOutcomeToCSV(cascadeValues, 'DALYs')    
     
+# getting coverage info
+import pickle    
+import data 
+import costcov
+costCov = costcov.Costcov()
+thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem)
+spreadsheetData = data.readSpreadsheet(dataSpreadsheetName, thisOptimisation.helper.keyList)
 
+# at 50% budget
+# read the optimal budget allocations from file
+filename = '%s_cascade_%s_0.5.pkl'%(resultsFileStem, optimise)
+infile = open(filename, 'rb')
+allocation = pickle.load(infile)
+infile.close()
+# run the model with this allocation
+modelList = thisOptimisation.oneModelRunWithOutput(allocation)
+# get cost coverage and target pop to translate cost to coverage
+costCoverageInfo = thisOptimisation.getCostCoverageInfo()
+# target pop size when interventions are added comes after 12 timesteps 
+targetPopSize = optimisation.getTargetPopSizeFromModelInstance(dataSpreadsheetName, thisOptimisation.helper.keyList, modelList[11])
+# get coverages
+coverages1 = {}
+for i in range(0, len(spreadsheetData.interventionList)):
+    intervention = spreadsheetData.interventionList[i]
+    coverages1[intervention] = costCov.function(allocation[intervention], costCoverageInfo[intervention], targetPopSize[intervention]) / targetPopSize[intervention]
+
+# at 150% budget
+filename = '%s_cascade_%s_1.5.pkl'%(resultsFileStem, optimise)
+infile = open(filename, 'rb')
+allocation = pickle.load(infile)
+infile.close()
+modelList = thisOptimisation.oneModelRunWithOutput(allocation)
+costCoverageInfo = thisOptimisation.getCostCoverageInfo()
+targetPopSize = optimisation.getTargetPopSizeFromModelInstance(dataSpreadsheetName, thisOptimisation.helper.keyList, modelList[11])
+coverages2 = {}
+for i in range(0, len(spreadsheetData.interventionList)):
+    intervention = spreadsheetData.interventionList[i]
+    coverages2[intervention] = costCov.function(allocation[intervention], costCoverageInfo[intervention], targetPopSize[intervention]) / targetPopSize[intervention]
