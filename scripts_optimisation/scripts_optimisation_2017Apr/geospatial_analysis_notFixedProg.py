e0b0225c44fa50c5eddd2d2aec007c86dd88c4e9
Ruth Pearson <ruth.pearson8@gmail.com>
2017-05-09 14:49:08 +1000

    added stunting as outcome to GA analysis script

--- scripts_optimisation/scripts_optimisation_2017Apr/geospatial_analysis_notFixedProg.py
+++ scripts_optimisation/scripts_optimisation_2017Apr/geospatial_analysis_notFixedProg.py
@@ -34,7 +34,7 @@ for regionName in regionNameList:
     spreadsheetList.append(spreadsheet)
 
 numCores = 8  # need this number times the number of outcomes you're optimising for
-for optimise in ['thrive']:
+for optimise in ['thrive', 'stunting']:
     print 'running GA for:  ', optimise
 
     resultsFileStem = resultsPath + '/' + date + '/' + optimise + '/geospatialNotFixed/'
