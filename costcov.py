570a3d2881dea688df86e015edb91e3f9760dc61
Ruth Pearson <ruth.pearson8@gmail.com>
2017-05-16 16:30:05 +1000

    wrote function in costCov.py to plot cost coverage curves and wrote a script to do this for each region

--- costcov.py
+++ costcov.py
@@ -72,4 +72,39 @@ class Costcov():
         plt.xlabel('coverage')
         plt.title('INVERSE FUNCTION')
         plt.show()
+        
+    def plotCurves(self, popSize, costCovInfo, plotTitle):
+        import matplotlib.pyplot as plt
+        import numpy as np
+        #FUNCTION
+        xvals = np.arange(0, 1e7, 1000)
+        for intervention in costCovInfo.keys():
+        
+            s = costCovInfo[intervention]['saturation']
+            p = popSize[intervention]
+            u = costCovInfo[intervention]['unitcost']
+            y = []
+            for x in xvals:
+                y.append( (2.*s/(1.+np.exp(-2.*x/(s*u*p))) - s)*p )
+            plt.plot(xvals, y, label = intervention)
+        plt.xlabel('spending')
+        plt.ylabel('coverage')
+        plt.title('FUNCTION: '+plotTitle)
+        plt.legend(loc = 'upper center', bbox_to_anchor=(0.5, -0.15), ncol=1, fancybox=True, shadow=True)  #plt.legend()
+        plt.show()
+        #INVERSE FUNCTION
+        yvals = np.arange(0, 1e6, 1e2)
+        for intervention in costCovInfo.keys():
+            s = costCovInfo[intervention]['saturation']
+            p = popSize[intervention]
+            u = costCovInfo[intervention]['unitcost']
+            x = []
+            for y in yvals:
+                x.append( -0.5*p*s*u* np.log((s*p-y)/(s*p+y)))
+            plt.plot(yvals, x, label = intervention)
+        plt.ylabel('spending')
+        plt.xlabel('coverage')
+        plt.title('INVERSE FUNCTION: '+plotTitle)
+        plt.legend(loc = 'upper center', bbox_to_anchor=(0.5, -0.15), ncol=1, fancybox=True, shadow=True)  #plt.legend()
+        plt.show()    
  
