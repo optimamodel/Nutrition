4732a3a19f5c2aa9d0038372c78efe734a39190b
Ruth Pearson <ruth.pearson8@gmail.com>
2017-09-21 11:59:38 +1000

    anemia optimisation script

---
+++ scripts_optimisation/scripts_optimisation_2017Sep/national.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 25 2016
+
+@author: madhura
+"""
+
+import os, sys
+moduleDir = os.path.join(os.path.dirname(__file__), '../..')
+sys.path.append(moduleDir)
+import optimisation
+
+
+rootpath = '../..'
+
+country = 'Bangladesh'
+date = '2017Aug'
+
+spreadsheet = rootpath+'/input_spreadsheets/'+country+'/'+date+'/InputForCode_'+country+'.xlsx'
+
+numModelSteps = 14
+MCSampleSize = 25
+cascadeValues = [0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0, 4.0]  
+haveFixedProgCosts = False
+numCores = 8
+
+for optimise in ['anemia frac everyone']:
+
+    resultsFileStem = rootpath+'/Results/'+date+'/'+optimise+'/national/'+country
+
+    thisOptimisation = optimisation.Optimisation(spreadsheet, numModelSteps, optimise, resultsFileStem)
+    thisOptimisation.performParallelCascadeOptimisation(MCSampleSize, cascadeValues, numCores, haveFixedProgCosts)
