eb1db5bdb0c1aa38e81691dd49664dd1db204774
Ruth Pearson <ruth.pearson8@gmail.com>
2017-10-03 12:58:18 +1100

    script for anemia demo optimisation

---
+++ scripts_results_harvest/harvest_2017Sep.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 22 12:13:34 2017
+
+@author: ruth
+"""
+
+import os, sys
+rootpath = '..'
+moduleDir = os.path.join(os.path.dirname(__file__), rootpath)
+sys.path.append(moduleDir)
+import optimisation
+
+country = 'Bangladesh'
+date = '2017Aug'
+
+numModelSteps = 14
+
+# NATIONAL
+dataSpreadsheetName = rootpath+'/input_spreadsheets/'+country+'/'+date+'/InputForCode_Bangladesh.xlsx'
+outcomeOfInterestList = ['anemia frac everyone', 'thrive', 'deaths']
+
+cascadeValues = [0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0, 4.0]
+optimise = 'anemia frac everyone'
+resultsFileStem = rootpath+'/Results/'+date+'/'+optimise+'/national/'+country
+# time series
+thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem)
+thisOptimisation.outputCurrentSpendingToCSV()
+for outcomeOfInterest in outcomeOfInterestList:
+    thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem)
+    thisOptimisation.outputCascadeAndOutcomeToCSV(cascadeValues, outcomeOfInterest)
