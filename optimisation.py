831516b0ec748c37e91e42e322810d4d59ee34f4
Ruth Pearson <ruth.pearson8@gmail.com>
2017-10-03 13:04:15 +1100

    changed hard coded regional budget back to what it should be (artifact of London meeting)

--- optimisation.py
+++ optimisation.py
@@ -798,7 +798,7 @@ class GeospatialOptimisation:
         # if BOCs not generated, generate them
         if self.regionalBOCs == None:
             self.generateAllRegionsBOC()
-        totalBudget = 1000000  #self.getTotalNationalBudget()
+        totalBudget = self.getTotalNationalBudget()
         scenarioMonteCarloOutput = []
         for r in range(0, geoMCSampleSize):
             proposalSpendingList = np.random.rand(self.numRegions)
