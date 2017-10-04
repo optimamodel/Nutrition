91b554bfe033e4354003faf4f3d6d192dc0197be
Ruth Pearson <ruth.pearson8@gmail.com>
2017-10-04 21:33:29 +1100

    changed for run

--- scripts_optimisation/scripts_optimisation_2017Sep/Tanzania/geospatial_deaths_progNotFixed.py
+++ scripts_optimisation/scripts_optimisation_2017Sep/Tanzania/geospatial_deaths_progNotFixed.py
@@ -26,11 +26,11 @@ MCSampleSize = 4
 #cascadeValues = [0.0, 0.1, 0.2, 0.4, 0.8, 1.0, 1.1, 1.25, 1.5, 1.7, 2.0] 
 cascadeValues = ['extreme']
 
-#regionNameList = ['Arusha', 'Dar_es_Salaam', 'Dodoma', 'Geita', 'Iringa', 'Kagera', 'Kaskazini_Pemba', 'Kaskazini_Unguja','Katavi']
+regionNameList = ['Arusha', 'Dar_es_Salaam', 'Dodoma', 'Geita', 'Iringa', 'Kagera', 'Kaskazini_Pemba', 'Kaskazini_Unguja','Katavi']
 #regionNameList = ['Kigoma', 'Kilimanjaro', 'Kusini_Pemba', 'Kusini_Unguja', 'Lindi', 'Manyara', 'Mara', 'Mbeya', 'Mjini_Magharibi']
 #regionNameList = ['Morogoro', 'Mtwara', 'Mwanza', 'Njombe', 'Pwani', 'Rukwa', 'Ruvuma', 'Shinyanga', 'Simiyu']
 #regionNameList = ['Singida', 'Tabora', 'Tanga']
-regionNameList = ['Arusha', 'Dar_es_Salaam', 'Dodoma', 'Geita', 'Iringa', 'Kagera',
+#regionNameList = ['Arusha', 'Dar_es_Salaam', 'Dodoma', 'Geita', 'Iringa', 'Kagera',
                   'Kaskazini_Pemba', 'Kaskazini_Unguja', 'Katavi', 'Kigoma', 'Kilimanjaro',
                   'Kusini_Pemba', 'Kusini_Unguja', 'Lindi', 'Manyara', 'Mara', 'Mbeya',
                   'Mjini_Magharibi', 'Morogoro', 'Mtwara', 'Mwanza', 'Njombe', 'Pwani',
