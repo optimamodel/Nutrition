a4d45d20b53c6f9bfe4d43967b66a109f02a6f12
Ruth Pearson <ruth.pearson8@gmail.com>
2017-10-04 16:07:02 +1100

    next set of regions

--- scripts_optimisation/scripts_optimisation_2017Sep/Tanzania/geospatial_thrive_progNotFixed.py
+++ scripts_optimisation/scripts_optimisation_2017Sep/Tanzania/geospatial_thrive_progNotFixed.py
@@ -18,8 +18,8 @@ numModelSteps = 180
 MCSampleSize = 4
 cascadeValues = [0.0, 0.1, 0.2, 0.4, 0.8, 1.0, 1.1, 1.25, 1.5, 1.7, 2.0] #, 'extreme']
 
-regionNameList = ['Arusha', 'Dar_es_Salaam', 'Dodoma', 'Geita', 'Iringa', 'Kagera', 'Kaskazini_Pemba', 'Kaskazini_Unguja','Katavi']
-#regionNameList = ['Kigoma', 'Kilimanjaro', 'Kusini_Pemba', 'Kusini_Unguja', 'Lindi', 'Manyara', 'Mara', 'Mbeya', 'Mjini_Magharibi']
+#regionNameList = ['Arusha', 'Dar_es_Salaam', 'Dodoma', 'Geita', 'Iringa', 'Kagera', 'Kaskazini_Pemba', 'Kaskazini_Unguja','Katavi']
+regionNameList = ['Kigoma', 'Kilimanjaro', 'Kusini_Pemba', 'Kusini_Unguja', 'Lindi', 'Manyara', 'Mara', 'Mbeya', 'Mjini_Magharibi']
 #regionNameList = ['Morogoro', 'Mtwara', 'Mwanza', 'Njombe', 'Pwani', 'Rukwa', 'Ruvuma', 'Shinyanga', 'Simiyu']
 #regionNameList = ['Singida', 'Tabora', 'Tanga']
 
