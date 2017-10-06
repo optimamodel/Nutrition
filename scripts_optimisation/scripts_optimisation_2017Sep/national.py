f1a82aa193457fb13bac31ba5464c39e38f1cfa6
Ruth Pearson <ruth.pearson8@gmail.com>
2017-10-06 16:29:56 +1100

    updated number of cores

--- scripts_optimisation/scripts_optimisation_2017Sep/national.py
+++ scripts_optimisation/scripts_optimisation_2017Sep/national.py
@@ -22,7 +22,7 @@ numModelSteps = 14
 MCSampleSize = 4
 cascadeValues = [0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0, 4.0, 8.0, 15.0, 30.0]  
 haveFixedProgCosts = False
-numCores = 8
+numCores = 11
 
 for optimise in ['anemia frac everyone']:
 
