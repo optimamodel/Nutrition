0ee9ee3a3c2ea17ff7f16c1c52eb74fe448d1976
Ruth Pearson <ruth.pearson8@gmail.com>
2017-10-10 16:03:39 +1100

    harvest scrip athena ready

--- scripts_results_harvest/harvest_2017Sep.py
+++ scripts_results_harvest/harvest_2017Sep.py
@@ -4,14 +4,19 @@ rootpath = '..'
 moduleDir = os.path.join(os.path.dirname(__file__), rootpath)
 sys.path.append(moduleDir)
 import optimisation
+import data
+import helper
+thisHelper = helper.Helper()
+import csv
+from multiprocessing import Process
 
 country = 'Tanzania'
 date = '2017Sep'
 spreadsheetDate = '2017Sep'
 numModelSteps = 180
 
-outcomeOfInterestList = ['thrive'] #['thrive', 'deaths']
-optimiseList = ['thrive'] #['deaths', 'thrive']
+outcomeOfInterestList = ['thrive', 'deaths']
+optimiseList = ['deaths', 'thrive']
 cascadeValues = [0.0, 0.1, 0.2, 0.4, 0.8, 1.0, 1.1, 1.25, 1.5, 1.7, 2.0, 'extreme']
 costCurveType = 'standard'
 GAFile = 'GA_progNotFixed'
@@ -32,10 +37,44 @@ for optimise in optimiseList:
     resultsFileStem = rootpath + '/Results/' + date + '/' + optimise + '/geospatialProgNotFixed/'
     geospatialOptimisation = optimisation.GeospatialOptimisation(spreadsheetList, regionNameList, numModelSteps, cascadeValues, optimise, resultsFileStem, costCurveType)
     geospatialOptimisation.outputRegionalCurrentSpendingToCSV()
-    #geospatialOptimisation.outputRegionalPostGAOptimisedSpendingToCSV(GAFile)
-    #geospatialOptimisation.outputRegionalBOCsFile('BOCs.csv')
+    geospatialOptimisation.outputRegionalPostGAOptimisedSpendingToCSV(GAFile)
     geospatialOptimisation.outputTradeOffCurves()
-    geospatialOptimisation.plotTradeOffCurves()
     for outcome in outcomeOfInterestList:
-        geospatialOptimisation.outputRegionalCascadesAndOutcomeToCSV(outcome)
+        prc = Process(target=geospatialOptimisation.outputRegionalCascadesAndOutcomeToCSV, args=(outcome))
+        prc.start()
+        #geospatialOptimisation.outputRegionalCascadesAndOutcomeToCSV(outcome)
+        
+        
+# get outcomes for current spending and zero spending
+spreadsheetData = data.readSpreadsheet(spreadsheetList[0], thisHelper.keyList)        
+zeroSpending = {}    
+for i in range(0, len(spreadsheetData.interventionList)):
+    intervention = spreadsheetData.interventionList[i]
+    zeroSpending[intervention] = 0.        
     
+outfilename = 'zero_spending.csv'  
+header1 = ['region', 'thrive', 'deaths', 'stunting prev']  
+with open(outfilename, "wb") as f:
+    writer = csv.writer(f)
+    writer.writerow(header1)
+    for region in range(len(regionNameList)):
+        regionName = regionNameList[region]
+        spreadsheet = spreadsheetList[region]
+        thisOptimisation = optimisation.Optimisation(spreadsheet, numModelSteps, 'dummy', resultsFileStem, costCurveType)    
+        modelList = thisOptimisation.oneModelRunWithOutput(zeroSpending)    
+        row =[regionName, modelList[numModelSteps-1].getOutcome('thrive'), modelList[numModelSteps-1].getOutcome('deaths'), modelList[numModelSteps-1].getOutcome('stunting prev')]
+        writer.writerow(row)
+
+outfilename = 'current_spending.csv'  
+header1 = ['region', 'thrive', 'deaths', 'stunting prev']  
+with open(outfilename, "wb") as f:
+    writer = csv.writer(f)
+    writer.writerow(header1)
+    for region in range(len(regionNameList)):
+        regionName = regionNameList[region]
+        spreadsheet = spreadsheetList[region]
+        thisOptimisation = optimisation.Optimisation(spreadsheet, numModelSteps, 'dummy', resultsFileStem, costCurveType)    
+        thisSpending = thisOptimisation.getInitialAllocationDictionary()        
+        modelList = thisOptimisation.oneModelRunWithOutput(thisSpending)    
+        row =[regionName, modelList[numModelSteps-1].getOutcome('thrive'), modelList[numModelSteps-1].getOutcome('deaths'), modelList[numModelSteps-1].getOutcome('stunting prev')]
+        writer.writerow(row)
