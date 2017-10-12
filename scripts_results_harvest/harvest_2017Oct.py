929ecf0ed38ac9d8c1427924eec33045bc70d1e1
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2017-10-12 17:23:25 +1100

    New harvest script

---
+++ scripts_results_harvest/harvest_2017Oct.py
@@ -0,0 +1,24 @@
+import os, sys
+rootpath = '..'
+moduleDir = os.path.join(os.path.dirname(__file__), rootpath)
+sys.path.append(moduleDir)
+import optimisation
+
+country = 'Bangladesh'
+date = '2017Oct'
+
+numModelSteps = 14
+
+# NATIONAL
+dataSpreadsheetName = rootpath+'/input_spreadsheets/'+country+'/'+date+'/InputForCode_Bangladesh.xlsx'
+outcomeOfInterestList = ['anemia frac everyone', 'thrive', 'deaths']
+
+cascadeValues = [0.25, 0.50, 0.75, 1.0, 1.50, 2.0, 3.0, 4.0, 8.0, 15.0, 30.0]
+optimise = 'anemia frac everyone'
+resultsFileStem = rootpath+'/Results/'+date+'/'+optimise+'/national/'+country
+# time series
+thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem)
+thisOptimisation.outputCurrentSpendingToCSV()
+for outcomeOfInterest in outcomeOfInterestList:
+    thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem)
+    thisOptimisation.outputCascadeAndOutcomeToCSV(cascadeValues, outcomeOfInterest)
