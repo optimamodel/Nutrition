6fc9d429bde9a3051359ee0c3ea4e5fce384c280
Ruth Pearson <ruth.pearson8@gmail.com>
2017-10-13 15:51:48 +1100

    scripts for extra cascade values

--- scripts_optimisation/scripts_optimisation_2017Sep/Tanzania/geospatial_deaths_progNotFixed.py
+++ scripts_optimisation/scripts_optimisation_2017Sep/Tanzania/geospatial_deaths_progNotFixed.py
@@ -23,13 +23,9 @@ nCores = 360 #(9*11)
 costCurveType = 'standard'
 numModelSteps = 180
 MCSampleSize = 4
-cascadeValues = [3., 4., 5., 6., 8., 10.] #[0.0, 0.1, 0.2, 0.4, 0.8, 1.0, 1.1, 1.25, 1.5, 1.7, 2.0, 'extreme'] 
+cascadeValues = [15.0, 20.0, 50.0, 100.0, 200.0, 400.0, 600.0] #[0.0, 0.1, 0.2, 0.4, 0.8, 1.0, 1.1, 1.25, 1.5, 1.7, 2.0, 3., 4., 5., 6., 8., 10., 'extreme'] 
 splitCascade = False
 
-#regionNameList = ['Arusha', 'Dar_es_Salaam', 'Dodoma', 'Geita', 'Iringa', 'Kagera', 'Kaskazini_Pemba', 'Kaskazini_Unguja','Katavi']
-#regionNameList = ['Kigoma', 'Kilimanjaro', 'Kusini_Pemba', 'Kusini_Unguja', 'Lindi', 'Manyara', 'Mara', 'Mbeya', 'Mjini_Magharibi']
-#regionNameList = ['Morogoro', 'Mtwara', 'Mwanza', 'Njombe', 'Pwani', 'Rukwa', 'Ruvuma', 'Shinyanga', 'Simiyu']
-#regionNameList = ['Singida', 'Tabora', 'Tanga']
 regionNameList = ['Arusha', 'Dar_es_Salaam', 'Dodoma', 'Geita', 'Iringa', 'Kagera',
                   'Kaskazini_Pemba', 'Kaskazini_Unguja', 'Katavi', 'Kigoma', 'Kilimanjaro',
                   'Kusini_Pemba', 'Kusini_Unguja', 'Lindi', 'Manyara', 'Mara', 'Mbeya',
