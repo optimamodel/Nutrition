a574b5ff377e96b77a79bd26bd21027df5da0f08
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2017-10-17 15:52:22 +1100

    Update to optimisation script

--- scripts_optimisation/scripts_optimisation_2017Oct/national_anemia_wasting.py
+++ scripts_optimisation/scripts_optimisation_2017Oct/national_anemia_wasting.py
@@ -7,19 +7,19 @@ import optimisation
 rootpath = '../..'
 
 country = 'Bangladesh'
-date = '2017Aug'
+date = '2017Oct'
 
 spreadsheet = rootpath+'/input_spreadsheets/'+country+'/'+date+'/InputForCode_'+country+'.xlsx'
 
 numModelSteps = 14
-MCSampleSize = 1
-cascadeValues = [1.]
+MCSampleSize = 15
+cascadeValues = [0.25, 0.75, 1.0, 2.0, 3.0, 4.0, 8.0, 15.0]
 haveFixedProgCosts = False
-numCores = 1
+numCores = 24
 
-for optimise in ['wasting prev']:
+for optimise in ['wasting prev', 'severely wasted prev', 'moderately wasted prev']:
 
-    resultsFileStem = rootpath+'/Results/'+date+'/'+optimise+'/national/'+country+'/'
+    resultsFileStem = rootpath+'/Results/'+date+'/'+optimise+'/national/'+country+'/'+country
 
     thisOptimisation = optimisation.Optimisation(spreadsheet, numModelSteps, optimise, resultsFileStem)
     thisOptimisation.performParallelCascadeOptimisation(MCSampleSize, cascadeValues, numCores, haveFixedProgCosts)
