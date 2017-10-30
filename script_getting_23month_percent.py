77418684297b00738fe7a64c896dad21bfaf75db
nickidge <nick.scott@burnet.edu.au>
2017-10-31 08:03:51 +1100

    Fix in script

--- script_getting_23month_percent.py
+++ script_getting_23month_percent.py
@@ -27,7 +27,7 @@ percentlist = []
 U5_init = []
 
 for region in range(len(regionNameList)):
-    spreadsheet = 'C:/Users/Nick/Desktop/Nutrition/input_spreadsheets/Tanzania/2017Sep/regions/' + 'InputForCode_' + regionNameList[region] + '.xlsx'
+    spreadsheet = 'C:/Users/Nick/Desktop/Nutrition/input_spreadsheets/Tanzania/2017Sep/regions/' + 'InputForCode_' + regionNameList[region] + '.xlsx'   
     spreadsheetData = data.readSpreadsheet(spreadsheet, helper.keyList)
 
     model, derived, params = helper.setupModelDerivedParameters(spreadsheetData)
@@ -49,16 +49,16 @@ for region in range(len(regionNameList)):
     pop_6_11.append(modelList[stepFinal].listOfAgeCompartments[2].getTotalPopulation())
     pop_12_23.append(modelList[stepFinal].listOfAgeCompartments[3].getTotalPopulation())
     pop_24_59.append(modelList[stepFinal].listOfAgeCompartments[4].getTotalPopulation())
-    print (pop_1 + pop_1_5 + pop_6_11 + pop_12_23) / pop_24_59
-    percentlist.append((pop_1 + pop_1_5 + pop_6_11 + pop_12_23) / pop_24_59)
-    U5_init.append(pop_1 + pop_1_5 + pop_6_11 + pop_12_23 + pop_24_59)
+    print ((pop_1[len(pop_1)-1] + pop_1_5[len(pop_1_5)-1] + pop_6_11[len(pop_6_11)-1] + pop_12_23[len(pop_12_23)-1]) / ((pop_1[len(pop_1)-1] + pop_1_5[len(pop_1_5)-1] + pop_6_11[len(pop_6_11)-1] + pop_12_23[len(pop_12_23)-1] + pop_24_59[len(pop_24_59)-1])))
+    percentlist.append((pop_1[len(pop_1)-1] + pop_1_5[len(pop_1_5)-1] + pop_6_11[len(pop_6_11)-1] + pop_12_23[len(pop_12_23)-1]) / ((pop_1[len(pop_1)-1] + pop_1_5[len(pop_1_5)-1] + pop_6_11[len(pop_6_11)-1] + pop_12_23[len(pop_12_23)-1] + pop_24_59[len(pop_24_59)-1])))
+    U5_init.append(pop_1[0] + pop_1_5[0] + pop_6_11[0] + pop_12_23[0] + pop_24_59[0])
 
 export = pd.DataFrame(regionNameList)
 export['population < 1 month'] = pd.DataFrame(pop_1)
 export['population 1-5 month'] = pd.DataFrame(pop_1_5)
 export['population 6-11 month'] = pd.DataFrame(pop_6_11)
 export['population 12-23 month'] = pd.DataFrame(pop_12_23)
-export['population 23-59 month'] = pd.DataFrame(pop_23_59)
+export['population 23-59 month'] = pd.DataFrame(pop_24_59)
 export['percent of U5 < 23 months'] = pd.DataFrame(percentlist)
 export['U5 total'] = pd.DataFrame(U5_init) 
 
