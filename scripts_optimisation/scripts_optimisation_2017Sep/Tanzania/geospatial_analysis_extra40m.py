4c6a26a575e350caf37b05bbc0d66df5c77b5107
Ruth Pearson <ruth.pearson8@gmail.com>
2017-11-07 17:14:26 +1100

    added analysis script to add 0m

---
+++ scripts_optimisation/scripts_optimisation_2017Sep/Tanzania/geospatial_analysis_extra40m.py
@@ -0,0 +1,44 @@
+rootpath = '../../..'
+
+import os, sys
+
+moduleDir = os.path.join(os.path.dirname(__file__), rootpath)
+sys.path.append(moduleDir)
+import optimisation
+from multiprocessing import Process
+
+country = 'Tanzania'
+date = '2017Sep'
+spreadsheetDate = '2017Sep'
+haveFixedProgCosts = False  # programs are not fixed, adding extra money automatically fixes regional spending and distributes the extra
+
+rerunMCSampleSize = 4
+geoMCSampleSize = 4
+numModelSteps = 180
+cascadeValues = [0.0, 0.1, 0.2, 0.4, 0.8, 1.0, 1.1, 1.25, 1.5, 1.7, 2.0, 3., 4., 5., 6., 8., 10., 15.0, 20.0, 50.0, 100.0, 'extreme']
+costCurveType = 'standard'
+regionNameList = ['Arusha', 'Dar_es_Salaam', 'Dodoma', 'Geita', 'Iringa', 'Kagera',
+                  'Kaskazini_Pemba', 'Kaskazini_Unguja', 'Katavi', 'Kigoma', 'Kilimanjaro',
+                  'Kusini_Pemba', 'Kusini_Unguja', 'Lindi', 'Manyara', 'Mara', 'Mbeya',
+                  'Mjini_Magharibi', 'Morogoro', 'Mtwara', 'Mwanza', 'Njombe', 'Pwani',
+                  'Rukwa', 'Ruvuma', 'Shinyanga', 'Simiyu', 'Singida', 'Tabora', 'Tanga']
+
+spreadsheetFileStem = rootpath + '/input_spreadsheets/' + country + '/' + spreadsheetDate + '/regions/InputForCode_'
+spreadsheetList = []
+for regionName in regionNameList:
+    spreadsheet = spreadsheetFileStem + regionName + '.xlsx'
+    spreadsheetList.append(spreadsheet)
+
+numCores = 30  # need this number times the number of outcomes you're optimising for
+extraMoney = 40000000
+for optimise in ['thrive', 'deaths']:
+    print 'running GA for:  ', optimise
+    resultsFileStem = rootpath + '/Results/' + date + '/' + optimise + '/geospatialProgNotFixed/'
+    GAFile = 'GA_progNotFixed'
+    geospatialOptimisation = optimisation.GeospatialOptimisation(spreadsheetList, regionNameList, numModelSteps,
+                                                                 cascadeValues, optimise, resultsFileStem, costCurveType)
+    # parallel for each optimise                                                             
+    prc = Process(target=geospatialOptimisation.performParallelGeospatialOptimisationExtraMoney, args=(geoMCSampleSize, rerunMCSampleSize, GAFile, numCores, extraMoney, haveFixedProgCosts))
+    prc.start()                                                                 
+                              
+    #geospatialOptimisation.performParallelGeospatialOptimisationExtraMoney(geoMCSampleSize, rerunMCSampleSize, GAFile, numCores, extraMoney, haveFixedProgCosts)
