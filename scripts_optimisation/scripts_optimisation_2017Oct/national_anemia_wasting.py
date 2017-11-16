83dbf2249240763bcb57296ffa15afa61f69f403
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2017-11-16 16:13:39 +1100

    Updates to national script

--- scripts_optimisation/scripts_optimisation_2017Oct/national_anemia_wasting.py
+++ scripts_optimisation/scripts_optimisation_2017Oct/national_anemia_wasting.py
@@ -12,14 +12,21 @@ date = '2017Oct'
 spreadsheet = rootpath+'/input_spreadsheets/'+country+'/'+date+'/InputForCode_'+country+'.xlsx'
 
 numModelSteps = 14
-MCSampleSize = 10
+MCSampleSize = 4
 cascadeValues = [0.25, 0.75, 1.0, 2.0, 3.0, 4.0, 8.0, 15.0, 30.0]
 haveFixedProgCosts = False
 numCores = 9
+
+costCurve = 'standard'
+interventionsToRemove = ['Public provision of complementary foods with iron',
+                         'Public provision of complementary foods with iron (malaria area)',
+                         'Iron and folic acid supplementation for pregnant women', 'Iron and folic acid supplementation for pregnant women (malaria area)',
+                         'Iron and iodine fortification of salt', 'Iron fortification of wheat flour', 'Iron fortification of maize',
+                         'Iron fortification of rice', 'IFA fortification of maize']
 
 for optimise in ['wasting_prev', 'MAM_prev']:
 
     resultsFileStem = rootpath+'/Results/'+date+'/'+optimise+'/national/'+country
 
-    thisOptimisation = optimisation.Optimisation(spreadsheet, numModelSteps, optimise, resultsFileStem)
+    thisOptimisation = optimisation.Optimisation(spreadsheet, numModelSteps, optimise, resultsFileStem, costCurve, interventionsToRemove=interventionsToRemove)
     thisOptimisation.performParallelCascadeOptimisation(MCSampleSize, cascadeValues, numCores, haveFixedProgCosts)
