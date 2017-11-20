b6ff8dc24a70fb036966539289a0d6ea59abe851
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2017-11-20 11:52:56 +1100

    Interventions to remove

--- scripts_optimisation/scripts_optimisation_2017Oct/national_anemia_wasting2.py
+++ scripts_optimisation/scripts_optimisation_2017Oct/national_anemia_wasting2.py
@@ -16,10 +16,17 @@ MCSampleSize = 10
 cascadeValues = [0.25, 0.75, 1.0, 2.0, 3.0, 4.0, 5.0, 8.0, 12.0]
 haveFixedProgCosts = False
 numCores = 9
+costCurve = 'standard'
+
+interventionsToRemove = ['Public provision of complementary foods with iron',
+                         'Public provision of complementary foods with iron (malaria area)',
+                         'Iron and folic acid supplementation for pregnant women', 'Iron and folic acid supplementation for pregnant women (malaria area)',
+                         'Iron and iodine fortification of salt', 'Iron fortification of wheat flour', 'Iron fortification of maize',
+                         'Iron fortification of rice', 'IFA fortification of maize']
 
 for optimise in ['SAM_prev', 'deaths']:
 
     resultsFileStem = rootpath+'/Results/'+date+'/'+optimise+'/national/'+country
 
-    thisOptimisation = optimisation.Optimisation(spreadsheet, numModelSteps, optimise, resultsFileStem)
+    thisOptimisation = optimisation.Optimisation(spreadsheet, numModelSteps, optimise, resultsFileStem, costCurve, interventionsToRemove=interventionsToRemove)
     thisOptimisation.performParallelCascadeOptimisation(MCSampleSize, cascadeValues, numCores, haveFixedProgCosts)
