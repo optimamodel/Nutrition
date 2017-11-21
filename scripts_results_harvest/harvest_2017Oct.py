f11ba859940ce25c70717b94528120c62ac28863
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2017-11-21 15:51:01 +1100

    Update to harvest script

--- scripts_results_harvest/harvest_2017Oct.py
+++ scripts_results_harvest/harvest_2017Oct.py
@@ -13,12 +13,12 @@ numModelSteps = 14
 dataSpreadsheetName = rootpath+'/input_spreadsheets/'+country+'/'+date+'/InputForCode_Bangladesh.xlsx'
 outcomeOfInterestList = ['wasting_prev', 'SAM_prev', 'MAM_prev', 'deaths']
 
-cascadeValues = [0.25, 0.75, 1.0, 2.0, 3.0, 4.0, 8.0, 15.0, 30.0]
+cascadeValues = [0.25, 0.75, 1.0, 2.0, 3.0, 4.0, 5.0, 8.0, 12.0]
 optimiseList = ['wasting_prev', 'SAM_prev', 'MAM_prev', 'deaths']
 for optimise in optimiseList:
-    resultsFileStem = rootpath+'/Results/'+date+'/'+optimise+'/national/'+country
+    resultsFileStem = rootpath+'/Results/'+date+'_New/'+optimise+'/national/'+country
     # time series
-    thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem)
+    thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem, 'dummyCurve')
     thisOptimisation.outputCurrentSpendingToCSV()
     for outcomeOfInterest in outcomeOfInterestList:
         thisOptimisation = optimisation.Optimisation(dataSpreadsheetName, numModelSteps, optimise, resultsFileStem, 'dummyCurve')
