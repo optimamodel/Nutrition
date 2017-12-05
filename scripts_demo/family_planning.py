e2be89bc2903b404115d78b9261a50b162d42a15
Ruth Pearson <ruth.pearson8@gmail.com>
2017-12-05 15:07:32 +1100

    updates to script demo FP

--- scripts_demo/family_planning.py
+++ scripts_demo/family_planning.py
@@ -21,15 +21,19 @@ m,d,p = thisHelper.setupModelDerivedParameters(thisData)
 numSteps = 13
 
 zero_cov = {}
-for intervention in thisData.effectivenessFP:
+for intervention in thisData.interventionCompleteList:
     zero_cov[intervention] = 0.0
     
-header = ['scenario', 'cumulative neonatal deaths', 'cumulative child deaths', 'cumulative PW deaths', 'cumulative births']    
+zeroDist = {}
+for intervention in thisData.effectivenessFP:
+    zeroDist[intervention] = 0.0    
+    
+header = ['scenario', 'cumulative neonatal deaths', 'cumulative child deaths', 'cumulative PW deaths', 'cumulative births', 'cumulative thrive']    
 
 # run baseline
 for i in range(numSteps + 1):
     m.moveModelOneYear()
-row0 = ['baseline', m.listOfAgeCompartments[0].getCumulativeDeaths(), m.getTotalCumulativeDeathsChildren(), m.getTotalCumulativeDeathsPW(), m.cumulativeBirths]
+row0 = ['baseline', m.listOfAgeCompartments[0].getCumulativeDeaths(), m.getTotalCumulativeDeathsChildren(), m.getTotalCumulativeDeathsPW(), m.cumulativeBirths, m.getOutcome('thrive')]
 
 # run for zero FP coverage
 m,d,p = thisHelper.setupModelDerivedParameters(thisData)
@@ -37,20 +41,41 @@ m.moveModelOneYear()
 m.updateCoverages(zero_cov)
 for i in range(numSteps):
     m.moveModelOneYear()
-row1 = ['zero', m.listOfAgeCompartments[0].getCumulativeDeaths(), m.getTotalCumulativeDeathsChildren(), m.getTotalCumulativeDeathsPW(), m.cumulativeBirths]
+row1 = ['zero', m.listOfAgeCompartments[0].getCumulativeDeaths(), m.getTotalCumulativeDeathsChildren(), m.getTotalCumulativeDeathsPW(), m.cumulativeBirths, m.getOutcome('thrive')]
 
 # run with each intervention scaled to 0.85
 rows = []
 for intervention in thisData.effectivenessFP:
+    thisData.distributionFP = dcp(zeroDist)
+    thisData.distributionFP[intervention] = 1.0
     m,d,p = thisHelper.setupModelDerivedParameters(thisData)
     m.moveModelOneYear()
-    cov = dcp(zero_cov)
-    cov[intervention] = 0.85
+    cov = {}
+    cov['Family Planning'] = 0.85
     m.updateCoverages(cov)
     for i in range(numSteps):
         m.moveModelOneYear()
-    row = [intervention + ' at 85%', m.listOfAgeCompartments[0].getCumulativeDeaths(), m.getTotalCumulativeDeathsChildren(), m.getTotalCumulativeDeathsPW(), m.cumulativeBirths]
+    row = [intervention + ' at 85%', m.listOfAgeCompartments[0].getCumulativeDeaths(), m.getTotalCumulativeDeathsChildren(), m.getTotalCumulativeDeathsPW(), m.cumulativeBirths, m.getOutcome('thrive')]
     rows.append(row)
+   
+   
+# varying FP coverages and unmet need
+thisData = data.readSpreadsheet(spreadsheet, thisHelper.keyList)   
+rows2 = []
+for covFP in [0.1, 0.3, 0.5, 0.6, 0.7, 0.85]:
+    rows2.append(['FP cov ' + str(covFP)])
+    for unmetNeed in [0.1, 0.3, 0.5]:
+        thisData.demographics['unmet need for family planning'] = unmetNeed
+        m,d,p = thisHelper.setupModelDerivedParameters(thisData)
+        m.moveModelOneYear()
+        cov = {}
+        cov['Family Planning'] = covFP
+        m.updateCoverages(cov)
+        for i in range(numSteps):
+            m.moveModelOneYear()
+        row = ['unmet need ' + str(unmetNeed), m.listOfAgeCompartments[0].getCumulativeDeaths(), m.getTotalCumulativeDeathsChildren(), m.getTotalCumulativeDeathsPW(), m.cumulativeBirths, m.getOutcome('thrive')]
+        rows2.append(row)
+        
     
 
 
@@ -60,5 +85,9 @@ with open(filename, 'wb') as f:
     w.writerow(header)
     w.writerow(row0)
     w.writerow(row1)
+    w.writerow('')
+    for row in rows2:
+        w.writerow(row)
+    w.writerow('')
     for row in rows:
         w.writerow(row)
