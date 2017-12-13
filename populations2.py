91140fce6703a8bdb34d37ff497d6bd32cf6d2b4
samh <sam.hainsworth@burnet.edu.au>
2017-12-13 12:00:02 +1100

    Calculate reference mortality in child population

--- populations2.py
+++ populations2.py
@@ -8,7 +8,7 @@ class Box:
 
 class AgeGroup:
     def __init__(self, age, populationSize, boxes):
-        self.age = age
+        self.name = age
         self.populationSize = populationSize
         self.boxes = boxes
 
@@ -20,6 +20,7 @@ class Population(object):
         self.childAges = project.childAges
         self.PWages = project.PWages
         self.WRAages = project.WRAages
+        self.risks = ['Stunting', 'Wasting', 'Breastfeeding', 'Anaemia']
         self.stuntingDist = project.riskDistributions['Stunting']
         self.wastingDist = project.riskDistributions['Wasting']
         self.bfDist = project.riskDistributions['Breastfeeding']
@@ -51,6 +52,7 @@ class Children(Population):
         self.ageSpans = [1., 5., 6., 12., 36.]
         self._makePopSizes()
         self._makeBoxes()
+        self._setChildrenReferenceMortality()
 
     def _makePopSizes(self):
         # for children less than 1 year, annual live births
@@ -79,6 +81,83 @@ class Children(Population):
                                       self.anaemiaDist[anaemiaCat][age]
                             boxes[stuntingCat][wastingCat][bfCat][anaemiaCat] = Box(thisPop)
             self.ageGroups.append(AgeGroup(age, popSize, boxes))
+
+    def _setChildrenReferenceMortality(self):
+        # Equation is:  LHS = RHS * X
+        # we are solving for X
+        # Calculate RHS for each age and cause
+        RHS = {}
+        for age in self.childAges:
+            RHS[age] = {}
+            for cause in self.project.causesOfDeath:
+                RHS[age][cause] = 0.
+                for stuntingCat in self.stuntingList:
+                    for wastingCat in self.wastingList:
+                        for bfCat in self.bfList:
+                            for anaemiaCat in self.anaemiaList:
+                                t1 = self.stuntingDist[stuntingCat][age]
+                                t2 = self.wastingDist[wastingCat][age]
+                                t3 = self.bfDist[bfCat][age]
+                                t4 = self.anaemiaDist[anaemiaCat][age]
+                                t5 = self.project.RRdeath['Stunting'][cause][stuntingCat][age]
+                                t6 = self.project.RRdeath['Wasting'][cause][wastingCat][age]
+                                t7 = self.project.RRdeath['Breastfeeding'][cause][bfCat][age]
+                                t8 = self.project.RRdeath['Anaemia'][cause][anaemiaCat][age]
+                                RHS[age][cause] += t1 * t2 * t3 * t4 * t5 * t6 * t7 * t8
+        # RHS for newborns only
+        age = '<1 month'
+        for cause in self.project.causesOfDeath:
+            RHS[age][cause] = 0.
+            for breastfeedingCat in self.bfList:
+                Pbf = self.bfDist[breastfeedingCat][age]
+                RRbf = self.project.RRdeath['Breastfeeding'][cause][breastfeedingCat][age]
+                for birthoutcome in self.birthOutcomes:
+                    Pbo = self.project.birthDist[birthoutcome]
+                    RRbo = self.project.RRdeath['Birth outcomes'][cause][birthoutcome]
+                    for anemiaStatus in self.anaemiaList:
+                        Pan = self.anaemiaDist[anemiaStatus][age]
+                        RRan = self.project.RRdeath['Anaemia'][cause][anemiaStatus][age]
+                        RHS[age][cause] += Pbf * RRbf * Pbo * RRbo * Pan * RRan
+        # calculate total mortality by age (corrected for units)
+        AgePop = []
+        for age in self.ageGroups:
+            AgePop.append(age.populationSize)
+        MortalityCorrected = {}
+        LiveBirths = self.project.demographics["number of live births"]
+        Mnew = self.project.mortalityRates["neonatal mortality"]
+        Minfant = self.project.mortalityRates["infant mortality"]
+        Mu5 = self.project.mortalityRates["under 5 mortality"]
+        # Newborns
+        ageName = self.ageGroups[0].name
+        m0 = Mnew * LiveBirths / 1000. / AgePop[0]
+        MortalityCorrected[ageName] = m0
+        # 1-5 months
+        ageName = self.ageGroups[1].name
+        m1 = (Minfant - Mnew) * LiveBirths / 1000. * 5. / 11. / AgePop[1]
+        MortalityCorrected[ageName] = m1
+        # 6-12 months
+        ageName = self.ageGroups[2].name
+        m2 = (Minfant - Mnew) * LiveBirths / 1000. * 6. / 11. / AgePop[2]
+        MortalityCorrected[ageName] = m2
+        # 12-24 months
+        ageName = self.ageGroups[3].name
+        m3 = (Mu5 - Minfant) * LiveBirths / 1000. * 1. / 4. / AgePop[3]
+        MortalityCorrected[ageName] = m3
+        # 24-60 months
+        ageName = self.ageGroups[4].name
+        m4 = (Mu5 - Minfant) * LiveBirths / 1000. * 3. / 4. / AgePop[4]
+        MortalityCorrected[ageName] = m4
+        # Calculate LHS for each age and cause of death then solve for X
+        Xdictionary = {}
+        for age in self.childAges:
+            Xdictionary[age] = {}
+            for cause in self.project.causesOfDeath:
+                LHS_age_cause = MortalityCorrected[age] * self.project.deathDist[cause][age]
+                Xdictionary[age][cause] = LHS_age_cause / RHS[age][cause]
+        self.referenceMortality = Xdictionary
+
+    #def setMortalityRates(self):
+
 
 
     def _getPopulation(self, ages, risks):
