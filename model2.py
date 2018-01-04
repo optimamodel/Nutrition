b5c144f3c6af48166ec8c4f0eb86d8b207557f29
samh <sam.hainsworth@burnet.edu.au>
2018-01-04 17:04:14 +1100

    Applying program updates to populations

--- model2.py
+++ model2.py
@@ -3,98 +3,114 @@ class Model:
     def __init__(self, filePath):
         import data2 as data
         import populations2 as pops
-        # import programs as progs
         import program_info
-        self.project = data.setUpProject(filePath)
+        self.project = data.setUpProject(filePath) # one modification comes below for IYCF target pops
+        self.populations = pops.setUpPopulations(self.project)
+        #self._setIYCFtargetPop(self.populations) # TODO: This is not complete
         self.programInfo = program_info.ProgramInfo(self.project)
-        self.populations = pops.setUpPopulations(self.project) # TODO: concerned about making dependence between populations and programs
-        self.year = None # TODO: get this from spreadsheet
-        self.newCoverages = None
 
-    # TODO: goal is to be able to call 'for population in populations', then 'population.update()' and the magic happens in populations module
+        self.year = None # TODO: get this from spreadsheet
+        self.newCoverages = self.project.costCurveInfo['baseline coverage']
+        # initialise baseline coverages
+        self._updateCoverages()
+
     # Pass in the program info as argument to the update method of populations
-    # TODO: self.programInfo contains the program objects etc, so that we can pass to the populations for updating.
 
 
     def _updateCoverages(self):
         for program in self.programInfo.programs:
-            program._updateCoverage(self.newCoverages[program.name])
+            program.updateCoverage(self.newCoverages[program.name], self.populations)
+
+    # TODO: TBC
+    def _setIYCFtargetPop(self, populations):
+        """
+        This method is placed here because population sizes are required for these calculations,
+        but are unknown until the population classes are initialised.
+        :return:
+        """
+        # get pop sizs for children & PW
+        allPopSizes = {}
+        for pop in populations[:2]: # TODO: make this a method in populations.py
+            allPopSizes.update({age.name:age.populationSize for age in pop.ageGroups})
+        targetPop = self.project.IYCFtargetPop
+        # equation: maxCovIYCF = sum(popSize * fracExposed) / sum(popSize)
+        # TODO: want this to be stratified by age (not mode)
+        # TODO: this calculation would be sum(fracExposed)/numModes (trivial case of more general equation).
+        maxCov = {}
+        for name, package in targetPop.iteritems():
+            maxCov[name] = 0.
+            for pop, modeFrac in package.iteritems():
+                for mode, frac in modeFrac.iteritems():
+                    maxCov[name] += frac * allPopSizes[pop] # TODO: should probably convert this to fraction (?)
+        # update in project
+        self.project.programTargetPop.update(maxCov)
 
 
-    def applyUpdates(self, newCoverages):
+
+    def applyNewProgramCoverages(self, newCoverages):
         '''newCoverages is required to be the overall coverage % (i.e. people covered / entire target pop) '''
         self.newCoverages = dcp(newCoverages)
         self._updateCoverages()
-        for pop in self.populations: # update all the populations
-            pop._update(self.programInfo)
+        for pop in self.populations[:1]: # update all the populations # TODO: UNDO SPLICING
+            self._updatePopulation(pop)
+
+    def _getApplicablePrograms(self, risk):
+        applicableProgNames = self.programInfo.programAreas[risk]
+        programs = list(filter(lambda x: x.name in applicableProgNames, self.programInfo.programs))
+        return programs
+
+    def _combineUpdates(self, updates, risk):
+        pass
+
+    def _updateDistributions(self, totalUpdate, risk):
+        pass
+
+    def _updatePopulation(self, population):
+        for risk in self.programInfo.programAreas.keys():
+            # get relevant programs, determined by risk area
+            applicableProgs = self._getApplicablePrograms(risk)
+            # TODO: could get applicable ageGroups here from a new sheet which maps pop and risks.
+            for ageGroup in population.ageGroups:
+                updates = []
+                for program in applicableProgs: # TODO: could check if worth considering an ageGroup by looking at 'applicableAgeGroups' in program attribute.
+                    print program.name
+                    if risk == 'Stunting':
+                        program._getStuntingUpdate(ageGroup)
+                    elif risk == 'Anaemia':
+                        program._getAnaemiaUpdate(ageGroup)
+                    elif risk == 'Wasting prevention':
+                        program._getWastingUpdateFromWastingIncidence(ageGroup)
+                    elif risk == 'Wasting treatment':
+                        program._getWastingPrevalenceUpdate(ageGroup)
+                    # elif risk == 'Breastfeeding':
+                    # elif risk == 'Diarrhoea':  # TODO: these updates which don't directly impact distributions will have to be treated carefully
+                    # elif risk == 'Mortality':
+                    # elif risk == 'Birth outcomes':
+                    # elif risk == 'Family planning':
+                    # elif risk == 'None':
+                    #     pass
+                    else:
+                        print "::risk not found for updating::"
+
+                # AT THIS POINT THIS AGE GROUP WILL HAVE THE TOTAL UPDATE FOR A PARTICULAR RISK
+                # TODO: combine the updates above in a Model method which also updates distributions
+                totalUpdate = self._combineUpdates(updates, risk) # TODO: here we can account for MAMtoSAM & SAMtoMAM updates for wasting
+                self._updateDistributions(totalUpdate, risk)
+
+                # TODO: need to update distributions etc here.
+                # TODO: update mortality
+                # UPDATE STUNTING DISTRIBUTION
+                # oldProbStunting = ageGroup.getFracRisk(risk)
+                # newProbStunting = oldProbStunting * update
+                # ageGroup.restratify(newProbStunting) # write this
+                # redistribute population based on new distribution
+                # ageGroup.redistribute() # use distributions of ageGroups
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-        # TODO: these functions may now have been superseded.
-        # stuntingUpdate = self._getStuntingUpdate()
-        # anaemiaUpdate = self._getAnaemiaUpdate()
-        # wastingUpdate = self._getWastingUpdate()
-
-
-
-
-    # def _getStuntingUpdate(self):
-    #     stuntingProgs = self.programs['Stunting']
-    #     ageGroups = self.children.ageGroups
-    #     update = [program._getStuntingUpdate(ageGroups, self.newCoverages) for program in stuntingProgs]
-    #     combinedUpdate = self._multiplyProgramUpdates(update)
-    #     return combinedUpdate
-    #
-    # def _getAnaemiaUpdate(self):
-    #     anaemiaProgs = self.programs['Anaemia']
-    #     ageGroups = [pop.ageGroups for pop in self.allPops]
-    #     update = [program._getAnaemiaUpdate(ageGroups, self.newCoverages) for program in anaemiaProgs]
-    #     combined = self._multiplyProgramUpdates(update)
-    #     return combined
-    #
-    # def _getWastingUpdate(self):
-    #     wastingProgs = self.programs['Wasting']
-    #     ageGroups = self.children.ageGroups
-    #     update = [program._getWastingPrevalenceUpdate(ageGroups, self.newCoverages) for program in wastingProgs]
-    #     combined = self._multiplyWastingUpdate(update)
-    #     return combined
-    #
-    # def _multiplyProgramUpdates(self, update):
-    #     '''Update is a list of lists, with each sub-list being updates for eacg age from a given program'''
-    #     # TODO: this & the wasting version below approach should be re-thought.
-    #     ageUpdate = {}
-    #     for progUpdate in update:
-    #         for age, value in progUpdate.iteritems():
-    #             if ageUpdate.get(age) is None:
-    #                 ageUpdate[age] = 1.
-    #             ageUpdate[age] *= value
-    #     return ageUpdate
-    #
-    # def _multiplyWastingUpdate(self, update):
-    #     ageUpdate = {}
-    #     for progUpdate in update:
-    #         for age, catValue in progUpdate.iteritems():
-    #             ageUpdate[age] = {}
-    #             for cat, value in catValue.iteritems():
-    #                 if ageUpdate[age].get(cat) is None:
-    #                     ageUpdate[age][cat] = 1.
-    #                 ageUpdate[age][age] *= value
-    #     return ageUpdate
 
 
 
