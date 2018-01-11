640e59680ce3f289df7b4d14c1dd0e9c64ed0ecb
Ruth Pearson <ruth.pearson8@gmail.com>
2018-01-11 15:59:16 +1100

    mega optimisation script ready for first go

--- scripts_optimisation/scripts_optimisation_2018Jan/all_interventions_version2.py
+++ scripts_optimisation/scripts_optimisation_2018Jan/all_interventions_version2.py
@@ -16,16 +16,16 @@ country = 'Bangladesh'
 date = '2018Jan'
 sheetDate = '2017Nov'
 spreadsheet = rootpath+'/input_spreadsheets/'+country+'/'+sheetDate+'/InputForCode_'+country+'.xlsx'
-
+totalBudget = 50000000
 cascadeValues = [0.25, 0.5, 1.0, 2.0, 3.0, 4.0, 8.0]
-objectiveList = ['thrive', 'deaths']
+objectiveList = ['thrive', 'deaths', 'anemia everyone', 'wasting_prev']
              
 # list of interventions not to be defunded             
 customInterventionList =['IPTp', 'Long-lasting insecticide-treated bednets', 'Family Planning']  
 
 resultsFileStem = rootpath+'/Results/'+country+'/national/'+date
 
-thisOptimisation = optimisation.Optimisation(cascadeValues, objectiveList, spreadsheet, resultsFileStem, country, parallel=True, numRuns=1)
+thisOptimisation = optimisation.Optimisation(cascadeValues, objectiveList, spreadsheet, resultsFileStem, country, totalBudget=totalBudget, parallel=True, numRuns=1)
 
 thisOptimisation.setCustomFixedCosts(customInterventionList)
 thisOptimisation.optimise()
