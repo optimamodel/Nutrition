410928963defc29439f5410d267b3e37b44949d7
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2018-01-22 13:35:18 +1100

    Objective & budget update for optimisation

--- scripts_optimisation/optimise_2018Jan/national_Tanzania.py
+++ scripts_optimisation/optimise_2018Jan/national_Tanzania.py
@@ -1,13 +1,11 @@
-import setup
 import optimisation3 as optimisation
 
-objectives = ['thrive']
-budgetMultiples = [1.]
+objectives = ['total_anaemia_prev', 'thrive', 'total_deaths', 'wasting_prev' ]
+budgetMultiples = [0.5, 1., 2., 4.]
 fileInfo = ['../../', '2018Jan', 'Tanzania']
-# totalBudget = 1647545487. # TODO: massive amount of money b/c of WASH -- what about MAM/SAM treatment... Looks weird
-totalBudget = 20000000.
-
+totalBudget = 1688794001. # calculated using reference case & IYCF 1 at 15% coverage. Fixed costs of reference will be subtracted from this
 
 if __name__ == '__main__':
     thisOptimisation = optimisation.Optimisation(objectives, budgetMultiples, fileInfo, totalBudget=totalBudget, numModelSteps=13, numRuns=1)
     thisOptimisation.optimise()
+    thisOptimisation.writeAllResults()
