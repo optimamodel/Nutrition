154133826ec08cf5cd78abc86153728d201559dd
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2018-01-22 14:15:17 +1100

    Added script directory

--- scripts_optimisation/optimise_2018Jan/national_Tanzania.py
+++ scripts_optimisation/optimise_2018Jan/national_Tanzania.py
@@ -1,8 +1,12 @@
+import os, sys
+root = '../..'
+moduleDir = os.path.join(os.path.dirname(__file__), root)
+sys.path.append(moduleDir)
 import optimisation3 as optimisation
 
 objectives = ['total_anaemia_prev', 'thrive', 'total_deaths', 'wasting_prev' ]
 budgetMultiples = [0.5, 1., 2., 4.]
-fileInfo = ['../../', '2018Jan', 'Tanzania']
+fileInfo = [root, '2018Jan', 'Tanzania']
 totalBudget = 1688794001. # calculated using reference case & IYCF 1 at 15% coverage. Fixed costs of reference will be subtracted from this
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -9,8 +9,5 @@ def getFilePath(root, bookDate, country):
     moduleDir = os.path.join(os.path.dirname(__file__), root)
     sys.path.append(moduleDir)
     filePath = root + 'input_spreadsheets/' + country + '/' + bookDate + '/InputForCode_' + country + '.xlsx'
-    return filePath
-
-if __name__ == '__main__':
-    filePath = getFilePath(root='', bookDate='2017Nov', country='Bangladesh')
-    model = setUpModel(filePath)
+    resultsPath = root + '/Results/' + country + '/national/' + bookDate
+    return filePath, resultsPath
