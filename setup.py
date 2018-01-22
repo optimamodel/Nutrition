4a4c2122233b88bd00e66dca84684b105d9e50bf
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2018-01-22 14:18:48 +1100

    Fixed directory

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ def getFilePath(root, bookDate, country):
     import os, sys
     moduleDir = os.path.join(os.path.dirname(__file__), root)
     sys.path.append(moduleDir)
-    filePath = root + 'input_spreadsheets/' + country + '/' + bookDate + '/InputForCode_' + country + '.xlsx'
+    filePath = root + '/input_spreadsheets/' + country + '/' + bookDate + '/InputForCode_' + country + '.xlsx'
     resultsPath = root + '/Results/' + country + '/national/' + bookDate
     return filePath, resultsPath
