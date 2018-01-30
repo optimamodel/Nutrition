4168e1549c7ee87db37be1f376a0132f59b69ec9
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2018-01-30 17:24:09 +1100

    Setting up model

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 import model2
+from copy import deepcopy as dcp
 
-def setUpModel(filePath):
-    model = model2.Model(filePath)
+def setUpModel(filePath, optimise=False):
+    model = model2.Model(filePath, optimise=optimise) # model has already moved 1 year
     return model
 
 def getFilePath(root, bookDate, country):
     import os, sys
     moduleDir = os.path.join(os.path.dirname(__file__), root)
     sys.path.append(moduleDir)
-    filePath = root + '/input_spreadsheets/' + country + '/' + bookDate + '/InputForCode_' + country + '.xlsx'
+    filePath = root + 'input_spreadsheets/' + country + '/' + bookDate + '/InputForCode_' + country + '.xlsx'
     resultsPath = root + '/Results/' + country + '/national/' + bookDate
     return filePath, resultsPath
+
+
+if __name__ == '__main__':
+    filePath = getFilePath('', '', 'Master')[0]
+    model = setUpModel(filePath)
+    model.runSimulation()
+    print model.getOutcome('thrive')
