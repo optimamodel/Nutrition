79e8014301547d31178877a9aafb762bdfe2b896
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2018-02-25 20:57:23 +1100

    Updated for all progs (not outside malaria area due to area==1)

--- scripts_optimisation/optimise_2018Feb/national_Tanzania.py
+++ scripts_optimisation/optimise_2018Feb/national_Tanzania.py
@@ -4,9 +4,9 @@ moduleDir = os.path.join(os.path.dirname(__file__), root)
 sys.path.append(moduleDir)
 import optimisation3 as optimisation
 
-objectives = ['healthy_children', 'thrive', 'total_deaths']
+objectives = ['healthy_children', 'thrive', 'total_deaths', 'total_anaemia_prev']
 budgetMultiples = [0.5, 1., 2., 3., 6.]
-fileInfo = [root, '2018Jan', 'Tanzania', 'subsetProgs']
+fileInfo = [root, '2018Jan', 'Tanzania', 'allProgs']
 totalBudget = 10000000.
 
 if __name__ == '__main__':
