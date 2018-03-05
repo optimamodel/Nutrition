672d84c84c63d998e816d4d50b9eac9a737fa36b
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2018-03-05 13:55:45 +1100

    Updated objectives and budgets

--- scripts_optimisation/optimise_2018Feb/national_Tanzania.py
+++ scripts_optimisation/optimise_2018Feb/national_Tanzania.py
@@ -4,10 +4,10 @@ moduleDir = os.path.join(os.path.dirname(__file__), root)
 sys.path.append(moduleDir)
 import optimisation3 as optimisation
 
-objectives = ['thrive', 'healthy_children', 'mortality_rate', 'total_anaemia_prev', 'wasting_prev']
-budgetMultiples = [0.5, 1., 2., 3., 6.]
+objectives = ['thrive', 'healthy_children', 'mortality_rate_children', 'mortality_rate_PW', 'total_anaemia_prev', 'wasting_prev']
+budgetMultiples = [1., 2., 4., 6., 8., 10.]
 fileInfo = [root, '2018Jan', 'Tanzania', 'allProgs']
-totalBudget = 10000000.
+totalBudget = 1e7
 
 if __name__ == '__main__':
     thisOptimisation = optimisation.Optimisation(objectives, budgetMultiples, fileInfo, totalBudget=totalBudget, filterProgs=True)
