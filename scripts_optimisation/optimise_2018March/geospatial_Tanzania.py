cc21d6fd8d3432a48e52645553d1cf7aee68db39
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2018-03-30 16:50:49 +1100

    String fix

--- scripts_optimisation/optimise_2018March/geospatial_Tanzania.py
+++ scripts_optimisation/optimise_2018March/geospatial_Tanzania.py
@@ -4,7 +4,6 @@ moduleDir = os.path.join(os.path.dirname(__file__), root)
 sys.path.append(moduleDir)
 import optimisation3 as optimisation
 
-
 regions = ['Arusha', 'Dar_es_Salaam', 'Dodoma', 'Kaskazini_Pemba', 'Kaskazini_Unguja', 'Katavi',
            'Kilimanjaro', 'Kusini_Pemba', 'Kusini_Unguja', 'Lindi', 'Manyara', 'Mara', 'Mjini_Magharibi',
            'Morogoro', 'Mtwara', 'Pwani', 'Rukwa', 'Ruvuma', 'Simiyu', 'Singida', 'Tabora', 'Tanga']
