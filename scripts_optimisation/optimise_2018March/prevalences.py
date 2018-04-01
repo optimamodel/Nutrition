76f333adb7c0b7a0138a7631462d0cfc73369f8e
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2018-04-01 12:21:51 +1000

    Scripts to get prevalences and scaled unit costs

---
+++ scripts_optimisation/optimise_2018March/prevalences.py
@@ -0,0 +1,26 @@
+import os, sys
+root = '../..'
+moduleDir = os.path.join(os.path.dirname(__file__), root)
+sys.path.append(moduleDir)
+import play
+from csv import writer
+
+regions = ['Arusha', 'Dar_es_Salaam', 'Dodoma', 'Kaskazini_Pemba', 'Kaskazini_Unguja', 'Katavi',
+           'Kilimanjaro', 'Kusini_Pemba', 'Kusini_Unguja', 'Lindi', 'Manyara', 'Mara', 'Mjini_Magharibi',
+           'Morogoro', 'Mtwara', 'Pwani', 'Rukwa', 'Ruvuma', 'Simiyu', 'Singida', 'Tabora', 'Tanga']
+
+outcomes = ['wasting_prev', 'anaemia_prev_children', 'anaemia_prev_PW', 'anaemia_prev_WRA']
+
+with open('prevalences.csv', 'wb') as f:
+    w = writer(f)
+    w.writerow(['Region'] + outcomes)
+    for region in regions:
+        filePath = play.getFilePath(root, 'Tanzania/regions', region)
+        model = play.setUpModel(filePath)
+        row = [region]
+        for outcome in outcomes:
+            row.append(model.getOutcome(outcome))
+        w.writerow(row)
+
+
+
