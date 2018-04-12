cb435dfd54e41225ece31255a4dc81cba59c6091
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2018-04-12 16:47:14 +1000

    Restructured file organisations

--- Projects/Tanzania/optimisations/geospatial_Tanzania.py
+++ Projects/Tanzania/optimisations/geospatial_Tanzania.py
@@ -1,9 +1,13 @@
 import os, sys
-root = '../..'
+root = '..'
 moduleDir = os.path.join(os.path.dirname(__file__), root)
 sys.path.append(moduleDir)
 import optimisation
 
+# regions = ['Arusha', 'Dar_es_Salaam', 'Dodoma', 'Kaskazini_Pemba', 'Kaskazini_Unguja', 'Katavi',
+#            'Kilimanjaro', 'Kusini_Pemba', 'Kusini_Unguja', 'Lindi', 'Manyara', 'Mara', 'Mjini_Magharibi',
+#            'Morogoro', 'Mtwara', 'Pwani', 'Rukwa', 'Ruvuma', 'Simiyu', 'Singida', 'Tabora', 'Tanga']
+regions = ['Arusha']
 objectives = ['healthy_children']
 fileInfo = [root, 'Tanzania']
 
