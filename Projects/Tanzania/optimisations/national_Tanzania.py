0090915c73de05b83c0cc54806991e6480a5dab0
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2018-04-13 12:34:38 +1000

    Updated Tanzania book to include expenditure tab

--- Projects/Tanzania/optimisations/national_Tanzania.py
+++ Projects/Tanzania/optimisations/national_Tanzania.py
@@ -1,15 +1,16 @@
 import os, sys
-root = '../../'
+root = '..'
 moduleDir = os.path.join(os.path.dirname(__file__), root)
 sys.path.append(moduleDir)
 import optimisation
 
-objectives = ['thrive', 'healthy_children', 'mortality_rate_children', 'mortality_rate_PW', 'total_anaemia_prev', 'wasting_prev']
+objectives = ['healthy_children']
 budgetMultiples = [1., 2., 4., 6., 8., 10.]
-fileInfo = [root, '2018Jan', 'Tanzania', '']
-totalBudget = 1e7
+fileInfo = [root, 'national', 'Tanzania', '']
+additionalFunds = 1e7
 
 if __name__ == '__main__':
-    thisOptimisation = optimisation.Optimisation(objectives, budgetMultiples, fileInfo, totalBudget=totalBudget, filterProgs=True)
+    thisOptimisation = optimisation.Optimisation(objectives, budgetMultiples, fileInfo,
+                                                 additionalFunds=additionalFunds, filterProgs=True)
     thisOptimisation.optimise()
     thisOptimisation.writeAllResults()
