4f62880caf3523e2047e5e03018d010805ce8b77
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2018-05-03 15:56:11 +1000

    New objective

--- nutrition/optimisation.py
+++ nutrition/optimisation.py
@@ -604,7 +604,7 @@ class GeospatialOptimisation:
         print '...Generating BOCs... \n'
         regions = self.setUpRegions(objective, fixWithin, additionalFunds)
         jobs = self.getBOCjobs(regions, objective)
-        maxRegions = 4 # 1 objectives * 10 budgets * 4 regions = 40 processes
+        maxRegions = 2 # 1 objectives * 10 budgets * 2 regions = 20 processes
         runJobs(jobs, maxRegions)
 
     def interpolateBOCs(self, objective, fixBetween, additionalFunds):
