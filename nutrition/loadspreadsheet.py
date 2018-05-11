e0136c419c0b0482d82b6dd5e578d2563e43827e
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2018-05-11 16:44:13 +1000

    Define class attributes in constructor

--- nutrition/loadspreadsheet.py
+++ nutrition/loadspreadsheet.py
@@ -4,6 +4,24 @@ from pandas import ExcelFile
 
 class DefaultParams(object):
     def __init__(self):
+        self.impacted_pop = None
+        self.prog_areas = {}
+        self.pop_areas = {}
+        self.rr_death = {}
+        self.or_cond = {}
+        self.or_cond_bo = {}
+        self.or_wasting_prog = {}
+        self.rr_dia = None
+        self.or_stunting_prog = None
+        self.bo_progs = None
+        self.correct_bf = None
+        self.rr_anaem_prog = None
+        self.or_anaem_prog = None
+        self.child_progs = None
+        self.pw_progs = None
+        self.rr_age_order = None
+        self.rr_interval = None
+        # read data
         self.spreadsheet = ExcelFile('default_params.xlsx')
         self.read_spreadsheet()
 
@@ -29,7 +47,6 @@ class DefaultParams(object):
     def prog_risks(self):
         areas = self.read_sheet('Program risk areas', [0])
         booleanFrame = areas.isnull()
-        self.prog_areas = {}
         for program, areas in booleanFrame.iterrows():
             for risk, value in areas.iteritems():
                 if self.prog_areas.get(risk) is None:
@@ -40,7 +57,6 @@ class DefaultParams(object):
     def pop_risks(self):
         areas = self.read_sheet('Population risk areas', [0])
         booleanFrame = areas.isnull()
-        self.pop_areas = {}
         for program, areas in booleanFrame.iterrows():
             for risk, value in areas.iteritems():
                 if self.pop_areas.get(risk) is None:
@@ -50,94 +66,75 @@ class DefaultParams(object):
 
     def relative_risks(self):
         # risk areas hidden in spreadsheet (white text)
-        # TODO: RR for bf does not have oldest age group. May need it?
-        self.rr_death = {}
         # stunting
-        RRsheet = self.read_sheet('Relative risks', [0,1,2], skiprows=1)
-        rr = RRsheet.loc['Stunting'].to_dict('index')
+        rr_sheet = self.read_sheet('Relative risks', [0,1,2], skiprows=1)
+        rr = rr_sheet.loc['Stunting'].to_dict('index')
         self.rr_death['Stunting'] = self.make_dict2(rr)
         # wasting
-        RRsheet = self.read_sheet('Relative risks', [0,1,2], skiprows=28)
-        rr = RRsheet.loc['Wasting'].to_dict('index')
+        rr_sheet = self.read_sheet('Relative risks', [0,1,2], skiprows=28)
+        rr = rr_sheet.loc['Wasting'].to_dict('index')
         self.rr_death['Wasting'] = self.make_dict2(rr)
         # anaemia
-        RRsheet = self.read_sheet('Relative risks', [0,1,2], skiprows=55).dropna(axis=1, how='all')
-        rr = RRsheet.loc['Anaemia'].to_dict('index')
+        rr_sheet = self.read_sheet('Relative risks', [0,1,2], skiprows=55).dropna(axis=1, how='all')
+        rr = rr_sheet.loc['Anaemia'].to_dict('index')
         self.rr_death['Anaemia'] = self.make_dict2(rr)
         # breastfeeding
-        RRsheet = self.read_sheet('Relative risks', [0,1,2], skiprows=64)
-        rr = RRsheet.loc['Breastfeeding'].to_dict('index')
+        rr_sheet = self.read_sheet('Relative risks', [0,1,2], skiprows=64)
+        rr = rr_sheet.loc['Breastfeeding'].to_dict('index')
         self.rr_death['Breastfeeding'] = self.make_dict2(rr)
         # diarrhoea
-        RRsheet = self.read_sheet('Relative risks', [0,1,2], skiprows=103).dropna(axis=1, how='all')
-        rr = RRsheet.loc['Diarrhoea'].to_dict('index')
+        rr_sheet = self.read_sheet('Relative risks', [0,1,2], skiprows=103).dropna(axis=1, how='all')
+        rr = rr_sheet.loc['Diarrhoea'].to_dict('index')
         self.rr_dia = self.make_dict2(rr)['None']
 
     def odds_ratios(self):
-        ORsheet = self.read_sheet('Odds ratios', [0,1], skiprows=1)
-        self.or_cond = {}
-        self.or_cond['Stunting'] = ORsheet.loc['Condition'].to_dict('index')
-        self.or_cond['Wasting'] = ORsheet.loc['Wasting'].to_dict('index')
-        self.or_cond['Anaemia'] = ORsheet.loc['Anaemia'].to_dict('index')
-        self.or_stunting_prog = ORsheet.loc['By program'].to_dict('index')
-
-    def make_dict2(self, mydict):
-        resultDict = {}
-        for key in mydict.keys():
-            first = key[0]
-            sec = key[1]
-            if resultDict.get(first) is None:
-                resultDict[first] = {}
-                resultDict[first][sec] = {}
-                resultDict[first][sec] = mydict[key]
-            if resultDict[first].get(sec) is None:
-                resultDict[first][sec] = {}
-                resultDict[first][sec] = mydict[key]
-        return resultDict
+        or_sheet = self.read_sheet('Odds ratios', [0,1], skiprows=1)
+        self.or_cond['Stunting'] = or_sheet.loc['Condition'].to_dict('index')
+        self.or_cond['Wasting'] = or_sheet.loc['Wasting'].to_dict('index')
+        self.or_cond['Anaemia'] = or_sheet.loc['Anaemia'].to_dict('index')
+        self.or_stunting_prog = or_sheet.loc['By program'].to_dict('index')
 
     def bo_progs(self):
-        BOprograms = self.read_sheet('Programs birth outcomes', [0,1], 'index')
-        newBOprograms = {}
-        for program in BOprograms.keys():
-            if not newBOprograms.get(program[0]):
-                newBOprograms[program[0]] = {}
-            newBOprograms[program[0]][program[1]] = BOprograms[program]
-        self.BOprograms = newBOprograms
+        progs = self.read_sheet('Programs birth outcomes', [0,1], 'index')
+        newprogs = {}
+        for program in progs.keys():
+            if not newprogs.get(program[0]):
+                newprogs[program[0]] = {}
+            newprogs[program[0]][program[1]] = progs[program]
+        self.bo_progs = newprogs
 
     def correct_bf(self):
-        self.correctBF = self.read_sheet('Appropriate breastfeeding', [0], 'dict')['Practice']
+        self.correct_bf = self.read_sheet('Appropriate breastfeeding', [0], 'dict')['Practice']
 
     def anaemia_progs(self):
-        anaemiaSheet = self.read_sheet('Programs anemia', [0,1])
-        self.rr_anaem_prog = anaemiaSheet.loc['Relative risks of anaemia when receiving intervention'].to_dict(orient='index')
-        self.or_anaem_prog = anaemiaSheet.loc['Odds ratios of being anaemic when covered by intervention'].to_dict(orient='index')
+        anaem_sheet = self.read_sheet('Programs anemia', [0,1])
+        self.rr_anaem_prog = anaem_sheet.loc['Relative risks of anaemia when receiving intervention'].to_dict(orient='index')
+        self.or_anaem_prog = anaem_sheet.loc['Odds ratios of being anaemic when covered by intervention'].to_dict(orient='index')
 
     def wasting_progs(self):
-        self.ORwastingProgram = {}
         wastingSheet = self.read_sheet('Programs wasting', [0,1])
-        self.ORwastingProgram['SAM'] = wastingSheet.loc['Odds ratio of SAM when covered by program'].to_dict(orient='index')
-        self.ORwastingProgram['MAM'] = wastingSheet.loc['Odds ratio of MAM when covered by program'].to_dict(orient='index')
+        self.or_wasting_prog['SAM'] = wastingSheet.loc['Odds ratio of SAM when covered by program'].to_dict(orient='index')
+        self.or_wasting_prog['MAM'] = wastingSheet.loc['Odds ratio of MAM when covered by program'].to_dict(orient='index')
 
     def child_progs(self):
         childSheet = self.read_sheet('Programs for children', [0,1,2])
         childDict = childSheet.to_dict(orient='index')
-        self.childPrograms = self.make_dict(childDict)
+        self.child_progs = self.make_dict(childDict)
 
     def pw_progs(self):
-        PWsheet = self.read_sheet('Programs for PW', [0,1,2])
-        PWdict = PWsheet.to_dict(orient='index')
-        self.PWprograms = self.make_dict(PWdict)
+        pw_sheet = self.read_sheet('Programs for PW', [0,1,2])
+        pw_dict = pw_sheet.to_dict(orient='index')
+        self.pw_progs = self.make_dict(pw_dict)
 
     def bo_risks(self):
-        BOsheet = self.read_sheet('Birth outcome risks', [0,1], skiprows=[0])
-        ors = BOsheet.loc['Odds ratios for conditions'].to_dict('index')
-        self.or_cond_bo = {}
+        bo_sheet = self.read_sheet('Birth outcome risks', [0,1], skiprows=[0])
+        ors = bo_sheet.loc['Odds ratios for conditions'].to_dict('index')
         self.or_cond_bo['Stunting'] = ors['Stunting (HAZ-score < -2)']
         self.or_cond_bo['MAM'] = ors['MAM (WHZ-score between -3 and -2)']
         self.or_cond_bo['SAM'] = ors['SAM (WHZ-score < -3)']
-        self.rr_age_order = BOsheet.loc['Relative risk by birth age and order'].to_dict('index')
-        self.rr_interval = BOsheet.loc['Relative risk by birth interval'].to_dict('index')
-        self.rr_death['Birth outcomes'] = BOsheet.loc['Relative risks of neonatal causes of death'].to_dict('index')
+        self.rr_age_order = bo_sheet.loc['Relative risk by birth age and order'].to_dict('index')
+        self.rr_interval = bo_sheet.loc['Relative risk by birth interval'].to_dict('index')
+        self.rr_death['Birth outcomes'] = bo_sheet.loc['Relative risks of neonatal causes of death'].to_dict('index')
 
     def iycf_effects(self): # TODO: IYCF packages to be defined in Project, based on user input (packages)
         effects = self.read_sheet('IYCF odds ratios', [0,1,2])
@@ -155,22 +152,38 @@ class DefaultParams(object):
         return df
 
     def make_dict(self, mydict):
-        '''myDict is a spreadsheet with 3 index cols, converted to dict using orient='index' '''
-        resultDict = {}
+        """ myDict is a spreadsheet with 3 index cols, converted to dict using orient='index' """
+        res_dict = {}
         for key in mydict.keys():
             first = key[0]
             sec = key[1]
             third = key[2]
-            if resultDict.get(first) is None:
-                resultDict[first] = {}
-                resultDict[first][sec] = {}
-                resultDict[first][sec][third] = mydict[key]
-            if resultDict[first].get(sec) is None:
-                resultDict[first][sec] = {}
-                resultDict[first][sec][third] = mydict[key]
-            if resultDict[first][sec].get(third) is None:
-                    resultDict[first][sec][third] = mydict[key]
-        return resultDict
+            if res_dict.get(first) is None:
+                res_dict[first] = {}
+                res_dict[first][sec] = {}
+                res_dict[first][sec][third] = mydict[key]
+            if res_dict[first].get(sec) is None:
+                res_dict[first][sec] = {}
+                res_dict[first][sec][third] = mydict[key]
+            if res_dict[first][sec].get(third) is None:
+                    res_dict[first][sec][third] = mydict[key]
+        return res_dict
+
+    def make_dict2(self, mydict):
+        """ myDict is a spreadsheet with 3 index cols, converted to dict using orient='index' """
+
+        res_dict = {}
+        for key in mydict.keys():
+            first = key[0]
+            sec = key[1]
+            if res_dict.get(first) is None:
+                res_dict[first] = {}
+                res_dict[first][sec] = {}
+                res_dict[first][sec] = mydict[key]
+            if res_dict[first].get(sec) is None:
+                res_dict[first][sec] = {}
+                res_dict[first][sec] = mydict[key]
+        return res_dict
 
 
 
