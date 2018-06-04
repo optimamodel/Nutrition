839cfd3b82812c4b2069cd76a9addd3542a3f06b
cliffckerr <questionably@gmail.com>
2018-06-04 01:48:19 -0700

    updated main

--- nutrition/webapp/main.py
+++ nutrition/webapp/main.py
@@ -1,10 +1,10 @@
 """
-main.py -- main module for HealthPrior webapp.
+main.py -- main module for the Optima Nutrition webapp.
     
-Last update: 6/1/18 (gchadder3)
+Last update: 2018jun04 by cliffk
 """
 
-import hptool as hp
+import nutrition as on
 import sciris.web as sw
 import sciris.core as sc
 
@@ -12,7 +12,7 @@ import sciris.core as sc
 def make_app():
 	# Create the ScirisApp object.  NOTE: app.config will thereafter contain all 
 	# of the configuration parameters, including for Flask.
-	app = sw.ScirisApp(__file__, app_config=hp.webapp.config)
+	app = sw.ScirisApp(__file__, app_config=on.webapp.config)
 
 	# RPC definitions
 	@app.register_RPC()
@@ -20,8 +20,8 @@ def make_app():
 		''' Return the information about the project. '''
 		gitinfo = sc.gitinfo(__file__)
 		version_info = {
-	        'version':   hp.version,
-	        'date':      hp.versiondate,
+	        'version':   on.version,
+	        'date':      on.versiondate,
 	        'gitbranch': gitinfo['branch'],
 	        'githash':   gitinfo['hash'],
 	        'gitdate':   gitinfo['date'],
@@ -29,7 +29,7 @@ def make_app():
 		return version_info
 
 	# Register the RPCs in the project.py module.
-	app.add_RPC_dict(hp.webapp.api.RPC_dict)
+	app.add_RPC_dict(on.webapp.api.RPC_dict)
 
 	return app
 
@@ -40,7 +40,7 @@ def run():
 	app = make_app()
 	
 	# Initialize the projects.
-	hp.webapp.api.init_projects(app)
+	on.webapp.api.init_projects(app)
 
 	# Run the client page with Flask and a Twisted server.
 	app.run_server()
