a61113391c769c13d24a52f097db106c186df69a
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2018-06-20 08:42:17 +1000

    not running default

--- tests/testoptims.py
+++ tests/testoptims.py
@@ -1,5 +1,15 @@
 import nutrition as on
 
-p = on.project.Project()
-p.default_optims(key='default', dorun=True)
-res = p.get_results('default')
+country = 'master'
+region = 'master'
+name = 'test'
+
+p = on.project.Project(name)
+
+defaults = on.data.OptimOptsTest(name, filepath=on.settings.test_opts_path())
+opts = [on.utils.OptimOpts(**defaults.get_attr())]
+optim_list = on.optimisation.make_optims(country, region, opts)
+p.add_optims(optim_list)
+
+p.run_optims()
+
