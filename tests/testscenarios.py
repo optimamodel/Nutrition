43e7f7667affdff548c2645eeb0815a587422170
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2018-06-22 13:28:08 +1000

    Minor update to test script

--- tests/testscenarios.py
+++ tests/testscenarios.py
@@ -1,11 +1,7 @@
 import nutrition as on
 
 p = on.project.default_project()
-on.scenarios.default_scens(p)
-
-p.run_scens()
+p.default_scens(dorun=True)
 res = p.get_results('default')
 
 allplots = on.plotting.make_plots(res, toplot=['prevs'])
-
-
