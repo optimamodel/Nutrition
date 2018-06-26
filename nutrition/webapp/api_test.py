5b8f167200d4eb55806af5466997b449670d6425
cliffckerr <questionably@gmail.com>
2018-06-26 13:20:07 +0300

    more tests

--- nutrition/webapp/api_test.py
+++ nutrition/webapp/api_test.py
@@ -4,12 +4,25 @@ Version:
 
 import sciris.core as sc
 import nutrition.ui as nu
+import os
 
 sc.tic()
 
-# Create blank and demo projects
+#%% Projects
+
+# Create a blank projects
+filename = 'temp_nutrition_project.prj'
 P = nu.Project(name='Blank')
+P.save(filename=filename)
+os.remove(filename)
+
+# Create a demo project
 D = nu.demo()
+
+#%% Scenarios
+
+#%% Optimizations
+
 
 
 sc.toc()
