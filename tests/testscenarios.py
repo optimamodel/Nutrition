f8a0054254030e7212a94fa1376bddf7031af811
cliffckerr <questionably@gmail.com>
2018-06-27 05:49:24 +0300

    modify testscenario to work with the new syntax

--- tests/testscenarios.py
+++ tests/testscenarios.py
@@ -2,5 +2,6 @@ import nutrition as on
 
 p = on.project.demo()
 p.run_scens()
-p.run_optims()
-res = p.get_results(['Scenarios'])
+#p.run_optims()
+p.plot(key='Scenarios', toplot='outputs')
+#res = p.get_results(['Scenarios'])
