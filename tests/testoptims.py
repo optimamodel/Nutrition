bcbaca6dd6347c22e578da5ea8f7f8e078bf9b40
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2018-07-26 14:32:06 +1000

    Some tests

--- tests/testoptims.py
+++ tests/testoptims.py
@@ -6,18 +6,27 @@ p = nu.Project('eg')
 p.load_data('demo', 'demo', name='eg')
 
 ## define custom optimization
-kwargs = {'name':'test',
+kwargs1 = {'name':'test1',
           'model_name': 'eg',
           'obj':'thrive',
           'mults':[1,2],
-          'prog_set': ['IYCF 1', 'Vitamin A supplementation', 'Micronutrient powders'],
-          'add_funds':10000000.}
+          'prog_set': ['Micronutrient powders', 'IYCF 1'],
+          'fix_curr': False,
+          'add_funds':10000000}
 
-optims = [Optim(**kwargs)]
+kwargs2 = {'name':'test2',
+          'model_name': 'eg',
+          'obj':'thrive',
+          'mults':[1,2],
+          'prog_set': ['Micronutrient powders', 'Vitamin A supplementation'],
+          'fix_curr': False,
+          'add_funds':10000000}
+
+optims = [Optim(**kwargs1), Optim(**kwargs2)]
 p.add_optims(optims)
-p.run_optims()
+p.run_optims(['test1'], swarmsize=1, maxiter=1, maxtime=1)
 
-p.plot(keys=['test'] ,optim=True)
+p.plot(keys=['test1'], optim=True)
 
 
 
