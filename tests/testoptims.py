31013a884aa5ee16383946dbb5a598c2faa39322
cliffckerr <questionably@gmail.com>
2018-08-03 06:08:35 +1000

    why is this not working

--- tests/testoptims.py
+++ tests/testoptims.py
@@ -1,8 +1,8 @@
 import nutrition.ui as nu
 from nutrition.optimization import Optim
 
-doplot = 0
-dosave = 1
+doplot = 1
+dosave = 0
 
 # load in data to create model
 p = nu.Project('eg')
@@ -13,7 +13,7 @@ kwargs1 = {'name':'test1',
           'model_name': 'eg',
           'obj':'thrive',
           'mults':[1,2],
-          'prog_set': ['Micronutrient powders', 'IYCF 1'],
+          'prog_set': ['Vitamin A supplementation', 'IYCF 1'], # ['Micronutrient powders', 'IYCF 1'],
           'fix_curr': False,
           'add_funds':10000000}
 
@@ -27,6 +27,6 @@ kwargs2 = {'name':'test2',
 
 optims = [Optim(**kwargs1), Optim(**kwargs2)]
 p.add_optims(optims)
-p.run_optims(['test2'], swarmsize=1, maxiter=1, maxtime=1)
-if doplot: p.plot(keys=['test2'], optim=True)
+p.run_optims(['test1'], swarmsize=5, maxiter=10, maxtime=10)
+if doplot: p.plot(keys=['test1'], optim=True)
 if dosave: p.write_results('optim_results.xlsx')
