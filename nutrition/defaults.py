6f839efdfb421dbe2832f77dd65a907eae689c6f
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2018-08-29 13:36:48 +1000

    Ability to change names

--- nutrition/defaults.py
+++ nutrition/defaults.py
@@ -7,24 +7,26 @@ Defaults correspond to the data set which is loaded by the user.
 """
 from .scenarios import make_scens
 
-def get_defaults(modelname, model):
+def get_defaults(modelname, model, basename='Baseline', zeroname='Zero cov'):
     """
     Assumes user has selected a data set to upload into the project.
     Both baseline and the zero scenario will be based on all the programs uploaded in the data book
     :param modelname: the name of the Model object
     :param model: a Model object for the baseline and zero coverage scenarios.
+    :param basename: the name of the baseline scenario
+    :param zeroname: name of the zero scenario
     :return: a list of default scenarios
     """
     # maintain current coverage
     progset = model.prog_info.base_progset()
     progvals = {prog:[] for prog in progset}
-    kwargs1 = {'name': 'Baseline',
+    kwargs1 = {'name': basename,
               'model_name': modelname,
               'scen_type': 'cov',
               'progvals': progvals}
     # scale down to zero coverage
     progvals = {prog: [0] for prog in progset}
-    kwargs2 = {'name': 'Zero cov',
+    kwargs2 = {'name': zeroname,
               'model_name': modelname,
               'scen_type': 'cov',
               'progvals': progvals}
