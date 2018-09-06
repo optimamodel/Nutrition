56a8c529762b335b5d1a82470587fe67936ca613
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2018-09-06 12:29:01 +1000

    Change plot start year

--- nutrition/plotting.py
+++ nutrition/plotting.py
@@ -14,6 +14,7 @@ if for_frontend:
 else:
     legend_loc = {'loc':'right'}
     ax_size = [0.2,0.10,0.65,0.75]
+    pltstart = 1
 
 
 def make_plots(all_res=None, toplot=None, optim=False):
@@ -112,7 +113,7 @@ def plot_outputs(all_res, seq, name):
                     ax.text(rect.get_x() + rect.get_width() / 2., height,'{}%'.format(change), ha='center',
                                 va='bottom')
         # formatting
-        title += ' %s \n %s-%s'%(utils.relabel(outcome).lower(), baseres.years[0], baseres.years[-1])
+        title += ' %s \n %s-%s'%(utils.relabel(outcome).lower(), baseres.years[pltstart], baseres.years[-1])
         sc.SIticks(ax=ax, axis='y')
         ax.set_ylim([0, ymax + ymax * .1])
         ax.set_ylabel('Number')
@@ -159,7 +160,7 @@ def plot_alloc(results, optim):
             bottom += spend
     ymax = max(bottom)
     if optim:
-        title = 'Optimal allocation, %s-%s'% (ref.years[0], ref.years[-1])
+        title = 'Optimal allocation, %s-%s'% (ref.years[pltstart], ref.years[-1])
         valuestr = str(results[1].prog_info.free / 1e6) # bit of a hack
         # format x axis
         if valuestr[1] == '.':
@@ -168,7 +169,7 @@ def plot_alloc(results, optim):
             valuestr = valuestr[:2]
         xlab = 'Total available budget (relative to US$%sM)' % valuestr
     else:
-        title = 'Average annual spending, %s-%s' % (ref.years[0], ref.years[-1])
+        title = 'Average annual spending, %s-%s' % (ref.years[pltstart], ref.years[-1])
         xlab = 'Scenario'
     ax.set_title(title)
     ax.set_xticks(x)
