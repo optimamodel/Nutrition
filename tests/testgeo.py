cbd8021f9fa535fe19c4136ddcb3e66c0ab34f82
SamuelHainsworth <sam.hainsworth@burnet.edu.au>
2018-09-25 17:04:07 +1000

    Change to test

--- tests/testgeo.py
+++ tests/testgeo.py
@@ -7,23 +7,23 @@ import sciris as sc
 p = nu.Project('Demo')
 p.load_data('demo', 'demoregion1', name='Demo1')
 p.load_data('demo', 'demoregion2', name='Demo2')
-# p.load_data('demo', 'demoregion2', name='Demo3')
-
-
+p.load_data('demo', 'demoregion2', name='Demo3')
 
 kwargs = {'name': 'test1',
-          'model_names': ['Demo1', 'Demo2'],
-          'region_names': ['demoregion1', 'demoregion2'],
+          'model_names': ['Demo1', 'Demo2', 'Demo3'],
+          'region_names': ['demoregion1', 'demoregion2', 'demoregion3'],
           'weights': 'thrive',
           'fix_curr': True,
-          'mults': [0,0.25, 0.75, 1],
+          'fix_within': False,
+          # todo: param for fixing funding amount, not program alloc
           'add_funds': 1e7,
           'prog_set': ['IFA fortification of maize', 'IYCF 1', 'Lipid-based nutrition supplements',
-           'Multiple micronutrient supplementation', 'Micronutrient powders', 'Kangaroo mother care',
-           'Public provision of complementary foods', 'Treatment of SAM',  'Vitamin A supplementation',
+                        'Multiple micronutrient supplementation', 'Micronutrient powders', 'Kangaroo mother care',
+                        'Public provision of complementary foods', 'Treatment of SAM',  'Vitamin A supplementation',
                        'Mg for eclampsia']}
+
 geo = Geospatial(**kwargs)
-results = p.run_geospatial(geo=geo, maxtime=10, maxiter=10, swarmsize=5)
-p.plot(optim=True)
+results = p.run_geospatial(geo=geo, maxtime=1, maxiter=1, swarmsize=1)
+p.plot(geo=True)
 import pylab as pl
 pl.show()
