a98348e1c413e7a512b8738f890c7dfdedaea1a9
cliffckerr <questionably@gmail.com>
2019-02-05 13:27:41 +1100

    version demonstrating the problem

--- tests/tmpdatabooktest2.py
+++ tests/tmpdatabooktest2.py
@@ -7,12 +7,11 @@ Created on Tue Feb  5 09:08:56 2019
 """
 
 #%%
-#from nutrition import ui as nu
-#proj = nu.demo()
-#proj.save('tmp.prj')
-
-import sciris as sc
-proj = sc.loadobj('tmp.prj')
+from nutrition import ui as nu
+proj = nu.demo()
+##proj.save('tmp.prj')
+#import sciris as sc
+#proj = sc.loadobj('tmp.prj')
 wb = proj.inputsheet()
-wb.writecells(sheetname='Baseline year population inputs', cells=[], vals=[], verbose=False, wbargs={'data_only': False})
+wb.writecells(sheetname='Nutritional status distribution', cells=['C5'], vals=[0.246], verbose=False, wbargs={'data_only': False})
 proj.load_data(fromfile=False)
