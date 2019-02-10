af588e29558e7c3fe161547231d2c4d85d41c9bd
Nick Scott <nick.scott@burnet.edu.au>
2019-02-10 15:46:17 +1100

    testing my server account

--- tests/testgeo.py
+++ tests/testgeo.py
@@ -4,21 +4,21 @@ from nutrition.geospatial import Geospatial
 # load in data to create model
 p = nu.Project('Demo')
 # three identical regions (same spreadsheet)
-p.load_data('demo', 'demoregion1', name='Demo1')
-p.load_data('demo', 'demoregion2', name='Demo2')
-p.load_data('demo', 'demoregion3', name='Demo3')
+p.load_data('demo', 'region1', name='Demo1')
+p.load_data('demo', 'region2', name='Demo2')
+p.load_data('demo', 'region3', name='Demo3')
 
 kwargs = {'name': 'test1',
           'modelnames': ['Demo1', 'Demo2', 'Demo3'],
           'weights': 'thrive',
           'fix_curr': False,
           'fix_regionalspend': False,
-          'add_funds': 5e6,
+          'add_funds': 0,
           'prog_set': ['IFA fortification of maize', 'IYCF 1', 'Lipid-based nutrition supplements',
                         'Multiple micronutrient supplementation', 'Micronutrient powders', 'Kangaroo mother care',
                         'Public provision of complementary foods', 'Treatment of SAM',  'Vitamin A supplementation',
                        'Mg for eclampsia', 'Zinc supplementation', 'Iron and iodine fortification of salt']}
 
 geo = Geospatial(**kwargs)
-results = p.run_geo(geo=geo, maxiter=4, swarmsize=4, maxtime=20, parallel=False)
-p.plot(geo=True)
+results = p.run_geo(geo=geo, maxiter=15, swarmsize=20, maxtime=140, dosave=True, parallel=False)
+#p.plot(toplot=['alloc'],geo=True)
