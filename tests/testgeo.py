bcf074570afbbfb7de0ca7e7c7e916bc123e81cc
George Chadderdon <gchadder3@gmail.com>
2019-02-19 15:41:17 +1100

    Changed test testgeo.py and testoptims.py to be more Windows friendly in parallel runs

--- tests/testgeo.py
+++ tests/testgeo.py
@@ -1,25 +1,26 @@
 import nutrition.ui as nu
 from nutrition.geospatial import Geospatial
 
-# load in data to create model
-p = nu.Project('Demo')
-# three identical regions (same spreadsheet)
-p.load_data('demo', 'region1', name='Demo1')
-p.load_data('demo', 'region2', name='Demo2')
-p.load_data('demo', 'region3', name='Demo3')
+if __name__ == '__main__':
+    # load in data to create model
+    p = nu.Project('Demo')
+    # three identical regions (same spreadsheet)
+    p.load_data('demo', 'region1', name='Demo1')
+    p.load_data('demo', 'region2', name='Demo2')
+    p.load_data('demo', 'region3', name='Demo3')
 
-kwargs = {'name': 'test1',
-          'modelnames': ['Demo1', 'Demo2', 'Demo3'],
-          'weights': 'thrive',
-          'fix_curr': False,
-          'fix_regionalspend': False,
-          'add_funds': 0,
-          'prog_set': ['IFA fortification of maize', 'IYCF 1', 'Lipid-based nutrition supplements',
-                        'Multiple micronutrient supplementation', 'Micronutrient powders', 'Kangaroo mother care',
-                        'Public provision of complementary foods', 'Treatment of SAM',  'Vitamin A supplementation',
-                       'Mg for eclampsia', 'Zinc supplementation', 'Iron and iodine fortification of salt']}
+    kwargs = {'name': 'test1',
+              'modelnames': ['Demo1', 'Demo2', 'Demo3'],
+              'weights': 'thrive',
+              'fix_curr': False,
+              'fix_regionalspend': False,
+              'add_funds': 0,
+              'prog_set': ['IFA fortification of maize', 'IYCF 1', 'Lipid-based nutrition supplements',
+                            'Multiple micronutrient supplementation', 'Micronutrient powders', 'Kangaroo mother care',
+                            'Public provision of complementary foods', 'Treatment of SAM',  'Vitamin A supplementation',
+                           'Mg for eclampsia', 'Zinc supplementation', 'Iron and iodine fortification of salt']}
 
-geo = Geospatial(**kwargs)
-results = p.run_geo(geo=geo, maxiter=15, swarmsize=20, maxtime=240, dosave=True, parallel=False)
-p.write_results('optim_results_geo.xlsx')
-p.plot(toplot=['alloc'],geo=True)
+    geo = Geospatial(**kwargs)
+    results = p.run_geo(geo=geo, maxiter=15, swarmsize=20, maxtime=240, dosave=True, parallel=False)
+    p.write_results('optim_results_geo.xlsx')
+    p.plot(toplot=['alloc'],geo=True)
