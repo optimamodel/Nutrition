a44acfd11cfba5c6047385a67731e690da152d8f
Nick Scott <nick.scott@burnet.edu.au>
2020-06-18 12:21:48 +1000

    results

--- nutrition/results.py
+++ nutrition/results.py
@@ -116,6 +116,7 @@ def write_results(results, projname=None, filename=None, folder=None):
         - sheet called 'budget and coverage' which contains all program cost and coverages over time """
     if projname is None: projname = ''
     outcomes = [
+        'Number of alive, non-stunted children turning age 5',
         'stunted', 'stunted_prev',
         'wasted', 'wasted_prev',
         'pw_anaemic', 'pw_anaemprev',
