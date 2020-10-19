4ed0aecd1ed04fe3f91e6af7af1108c916c4f5db
debra-tenbrink <41905633+debra-tenbrink@users.noreply.github.com>
2020-10-19 13:28:31 +0200

    Update optimization.py

--- nutrition/optimization.py
+++ nutrition/optimization.py
@@ -58,7 +58,7 @@ class Optim(sc.prettyobj):
 
     ######### OPTIMIZATION ##########
 
-    def run_optim(self, model, maxiter=20, swarmsize=25, maxtime=160, parallel=True, num_procs=None):
+    def run_optim(self, model, maxiter=80, swarmsize=35, maxtime=560, parallel=True, num_procs=None):
         if parallel:
             how = 'parallel'
             num_procs = num_procs if num_procs else self.num_cpus
