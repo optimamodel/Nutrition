3fbf6caed60e8dc8dcb903beeef6ae2da3ff5ef6
wptharindu <tharindu.wickram@burnet.edu.au>
2021-10-29 14:05:29 +1100

    I think the ramping is needed for "get_ann_covs" function as well.

--- tests/testscenarios.py
+++ tests/testscenarios.py
@@ -10,7 +10,7 @@ sys.setrecursionlimit(x)
 doplot = True
 resampling = False
 n_runs = 3
-pop_growth = "fixed budget"
+pop_growth = "fixed coverage"
 
 # load in data to create model
 """" If the model is run for a single run using 'p.run_scens()' then set resampling=False
