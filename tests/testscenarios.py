52e9a25e0d32af2dc456fcb1621d477cb734b2c4
wptharindu <tharindu.wickram@burnet.edu.au>
2021-11-08 13:21:20 +1100

    testing with options passed to scenario definitions

--- tests/testscenarios.py
+++ tests/testscenarios.py
@@ -10,20 +10,20 @@ sys.setrecursionlimit(x)
 doplot = True
 resampling = False
 n_runs = 3
-growth = "fixed budget"
 
 # load in data to create model
 """" If the model is run for a single run using 'p.run_scens()' then set resampling=False
     to make sure that the default point estimators are used from 
     the databook with out considering any randomness!"""
 p = nu.Project('eg')
-p.load_data('demo', 'testing', name='eg', resampling=resampling, growth=growth)
+p.load_data('demo', 'testing', name='eg', resampling=resampling)
 
 ### define custom scenarios
 kwargs1 = {'name':'Treat SAM 100%',
            'model_name': 'eg',
            'scen_type': 'coverage',
-            'progvals': sc.odict({'Treatment of SAM': [0.9, 0.5, 0.8]})}
+            'progvals': sc.odict({'Treatment of SAM': [0.9, 0.5, 0.8]}),
+            'growth': 'fixed budget'}
 
 kwargs2 = sc.dcp(kwargs1)
 kwargs2.update({'name': 'IYCF 1 100%',
@@ -33,7 +33,9 @@ kwargs3 = {'name': 'IYCF at $10 mil',
          'model_name': 'eg',
          'scen_type': 'budget',
            'progvals': sc.odict({'IYCF 1': [1e8, 2e8, 1.5e8, 2.5e8],
-                                 'IPTp': [2e7, 2.8e7, 2.8e7, 4.25e7]})}
+                                 'IPTp': [2e7, 2.8e7, 2.8e7, 4.25e7]}),
+           'growth': 'fixed budget',
+           'enforce_constraints_year': 2}
 
 ### testing FE bugs
 kwargs4 = {'name': 'FE check 1',
@@ -75,7 +77,8 @@ kwargs6 = {'name': 'Check bednets',
 kwargs7 = {'name': 'IYCF',
            'model_name': 'eg',
            'scen_type': 'coverage',
-           'progvals': sc.odict({'IYCF 1': [0.6, 0.2, 0.5, 0.95, 0.8]})}
+           'progvals': sc.odict({'IYCF 1': [0.6, 0.2, 0.5, 0.95, 0.8]}),
+           'growth': 'fixed budget'}
 
 kwargs8 = {'name': 'Treat SAM 100%',
                'model_name': 'Maximize thrive',
