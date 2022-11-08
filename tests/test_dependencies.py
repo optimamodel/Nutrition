db498b3e8a7edb4f39adfd5932c616261aa94429
nickidge <nick.scott@burnet.edu.au>
2022-11-09 10:52:36 +1100

    test dependencies

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,5 +1,6 @@
 import nutrition.ui as nu
 from nutrition.results import reduce_results, write_results
+from nutrition.optimization import Optim
 import sciris as sc
 import sys
 
@@ -9,7 +10,7 @@ doplot = False
 p = nu.Project("eg")
 p.load_data("demo", "testing", name="eg")
 
-### define custom scenarios
+### TEST MMS / IFAS dependency
 kwargs1 = {"name": "MMS",
            "model_name": "eg",
            "scen_type": "coverage",
@@ -37,17 +38,52 @@ kwargs4 = {"name": "MMS+IFAS budget",
            "progvals": sc.odict({"Multiple micronutrient supplementation": [8908763],
                                  "IFAS for pregnant women (community)": [1720475]}),
            "growth": "fixed coverage",
+           "enforce_constraints_year": 2}
+
+### TEST PPCF/LNS dependency
+
+kwargs5 = {"name": "PPCF",
+           "model_name": "eg",
+           "scen_type": "coverage",
+           "progvals": sc.odict({"Public provision of complementary foods": [0.95]}),
+           "growth": "fixed coverage",
            "enforce_constraints_year": 0}
+
+kwargs6 = {"name": "LNS",
+           "model_name": "eg",
+           "scen_type": "coverage",
+           "progvals": sc.odict({"Lipid-based nutrition supplements": [0.95]}),
+           "growth": "fixed coverage",
+           "enforce_constraints_year": 0}
+
+kwargs7 = {"name": "PPCF+LNS",
+           "model_name": "eg",
+           "scen_type": "coverage",
+           "progvals": sc.odict({"Public provision of complementary foods": [0.95],
+                                 "Lipid-based nutrition supplements": [0.95]}),
+           "growth": "fixed coverage",
+           "enforce_constraints_year": 1}
+
+### TEST dependency in optimisation
+kwargs8 = {"name": "MMS IFAS dep",
+           "model_name": "eg",
+           "mults": [1],
+           "weights": sc.odict({"Minimize the prevalence of ID anaemia in pregnant women": [1]}),
+           "prog_set": ["Multiple micronutrient supplementation", "IFAS for pregnant women (community)"],
+           "fix_curr": False,
+           "add_funds": 1e8} # lots of money
 
 if __name__ == "__main__":
 
     scen_list = nu.make_scens([kwargs1, kwargs2, kwargs3, kwargs4])
+    #scen_list = nu.make_scens([kwargs5, kwargs6, kwargs7])
     p.add_scens(scen_list)
+    results = p.run_scens(n_samples=0)
 
-    results = p.run_scens(n_samples=1)
-
-if doplot:
-    p.plot()
+    # optims = [Optim(**kwargs8)]
+    # p.add_optims(optims)
+    # results = p.run_optim(parallel=False)
+    # p.write_results("optim_results.xlsx")
 
 all_reduce = reduce_results(results)
 write_results(results=results, reduced_results=all_reduce, filename="scen_results_test.xlsx")
