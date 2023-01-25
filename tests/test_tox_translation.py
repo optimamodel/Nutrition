defdedf5e04a1292fb2b4f384ba95da8b3944438
Romesh Abeysuriya <romesh.abey@gmail.com>
2023-01-26 09:55:09 +1100

    Remove unused databooks (FE demo projects seem to work?)

--- tests/test_tox_translation.py
+++ tests/test_tox_translation.py
@@ -12,16 +12,16 @@ def test_translation_workflow():
     P = nu.Project("eg")
 
     # Check that loading a databook sets the project locale
-    P.load_data(inputspath=inputdir / "fr" / "demo_demo_input.xlsx")
+    P.load_data(inputspath=inputdir / "fr" / "demo_region1_input.xlsx")
     assert P.locale == "fr"
 
     # Once data is loaded, check that further data must have the same locale
     # This is to avoid mixing program names in different languages across regions
     with pytest.raises(Exception):
-        P.load_data(inputspath=inputdir / "en" / "demo_region1_input.xlsx")
+        P.load_data(inputspath=inputdir / "en" / "demo_region2_input.xlsx")
 
     # Check that we can continue loading the correct locale
-    P.load_data(inputspath=inputdir / "fr" / "demo_region1_input.xlsx")
+    P.load_data(inputspath=inputdir / "fr" / "demo_region2_input.xlsx")
 
 
 if __name__ == "__main__":
