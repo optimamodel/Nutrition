169e92ae7eef461291fd169834c241baa1c187a4
Nick Scott <nick.scott@burnet.edu.au>
2025-04-28 11:54:24 +1000

    Only prevention changes SAM treatment needs

--- nutrition/model.py
+++ nutrition/model.py
@@ -188,10 +188,19 @@ class Model(sc.prettyobj):
         Treatment of SAM is modelled to have a target population of all children.
         The unit cost is scaled based on treatment cost * SAM_prev * 2.6.
         This means that 100% coverage the total cost is treatment cost * SAM_prev * 2.6.
-        It also means the unit cost needs to be scaled dynamically as SAM prevalence changes
+        It also means the unit cost needs to be scaled dynamically as SAM incidence changes due to prevention scale-up.
+        An alternate implementation is to scale based on SAM prevalence, but this assumes incidence declines with treatment
         """
         if self.year>0:
-            self.prog_info.programs[_("Treatment of SAM")].unit_cost *= self.child_samprev[self.year] / self.child_samprev[self.year-1]
+            sam_cost_update_numerator = 0
+            sam_cost_update_denominator = 0
+            for a, age in enumerate(self.pops[0].age_groups):
+                sam_cost_update_numerator += (self.pops[0].age_groups[a].wastingPreventionUpdate[_("SAM")] *
+                                              self.pops[0].age_groups[a].bfUpdate[_("SAM")] *
+                                              self.pops[0].age_groups[a].diarrhoeaUpdate[_("SAM")]) * self.pops[0].age_groups[a].pop_size
+                sam_cost_update_denominator += self.pops[0].age_groups[a].pop_size
+            self.prog_info.programs[_("Treatment of SAM")].unit_cost *= sam_cost_update_numerator / sam_cost_update_denominator
+            #self.prog_info.programs[_("Treatment of SAM")].unit_cost *= self.child_samprev[self.year] / self.child_samprev[self.year-1]
 
     def _reset_storage(self):
         for pop in self.pops:
--- tests/test_SQLNS.py
+++ tests/test_SQLNS.py
@@ -33,11 +33,19 @@ kwargs3 = {"name": "TSAM",
                                  "Treatment of SAM": [0.19,0.37, 0.55,0.72,0.9,0.9,0.9,0.9,0.9]}),
            "growth": "fixed coverage"}
 
+kwargs4 = {"name": "TSAM+IYCF",
+           "model_name": "AGO",
+           "scen_type": "coverage",
+           "progvals": sc.odict({"Small quantity lipid-based nutrition supplements": [0.18,0.36,0.54,0.72,0.9,0.9,0.9,0.9,0.9,0.9],
+                                 "Treatment of SAM": [0.19,0.37, 0.55,0.72,0.9,0.9,0.9,0.9,0.9],
+                                 "IYCF 1": [0.126,0.37, 0.55,0.72,0.9,0.9,0.9,0.9,0.9],
+                                 "Vitamin A supplementation": [0.05,0.37, 0.55,0.72,0.9,0.9,0.9,0.9,0.9]}),
+           "growth": "fixed coverage"}
 
 
 if __name__ == "__main__":
 
-    scen_list = nu.make_scens([kwargs1, kwargs2, kwargs3])
+    scen_list = nu.make_scens([kwargs1, kwargs2, kwargs3, kwargs4])
     #scen_list = nu.make_scens([kwargs1, kwargs2, kwargs3, kwargs3a])
     p.add_scens(scen_list)
     results = p.run_scens(n_samples=0)
@@ -48,5 +56,5 @@ if __name__ == "__main__":
     # p.write_results("optim_results.xlsx")
 
 all_reduce = reduce_results(results)
-write_results(results=results, reduced_results=all_reduce, filename="scen_results_AGO2.xlsx")
+write_results(results=results, reduced_results=all_reduce, filename="scen_results_AGO3.xlsx")
 
