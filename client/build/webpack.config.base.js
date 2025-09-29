d69825b7127105c47456c1da77e9a484df703547
Romesh Abeysuriya <romesh.abey@gmail.com>
2025-09-30 06:47:18 +1000

    Add webpack 5 compatibility fix

--- client/build/webpack.config.base.js
+++ client/build/webpack.config.base.js
@@ -3,6 +3,7 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 
 function resolve (dir) {
@@ -81,6 +82,9 @@ module.exports = {
             }
           ]
         }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new webpack.ProvidePlugin({
+      process: 'process/browser'
+    })
   ]
 }
