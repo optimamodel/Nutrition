f767f3f4e66fb2c66d941153d6d3caefefb306a5
Romesh Abeysuriya <romesh.abey@gmail.com>
2025-10-04 06:40:22 +1000

    Update config

--- run_webapp.sh
+++ run_webapp.sh
@@ -2,4 +2,7 @@
 # This script can be used to build and run the nutrition app within a Docker container. Content will be stored
 # in `database/nutrition.db`
 sudo docker buildx build -t nutrition . < Dockerfile
-sudo docker run -p 80:80 -it -v database:/database nutrition:latest
+sudo docker run -p 80:80 -v database:/database nutrition:latest
+
+# Alternatively, to run an image from GitHub Container Registry:
+# sudo docker run -p 80:80 -v database:/database ghcr.io/optimamodel/nutrition:uv-docker
